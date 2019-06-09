bc421263ad47a36c4f5e1c864ec754073d3331bb
4slan <amalsago@student.42.fr>
2019-06-09 16:08:47 +0200

    handeling if there is no args

--- get_started/list_dir/main.c
+++ get_started/list_dir/main.c
@@ -6,34 +6,44 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 19:59:00 by amalsago          #+#    #+#             */
-/*   Updated: 2019/05/27 19:24:31 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/06/07 10:45:36 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <errno.h>
 
 int					main(int ac, char **av)
 {
-	DIR				*dirp;
-	struct dirent	*dirent;
+	int				i;
+	DIR				*dp;
+	struct dirent	*de;
 
-	if (ac < 2)
+	i = 0;
+	if (ac == 1)
 	{
-		printf("Usage: ./ft_list_dir dirname\n");
-		return (1);
+		dp = opendir(".");
+		while ((de = readdir(dp)) != NULL)
+			printf("%s\n", de->d_name);
+		closedir(dp);
 	}
-	dirp = opendir(av[1]);
-	if (dirp == NULL)
+	else
 	{
-		printf("Cannot open the directory\n");
-		return (1);
+		while (++i < ac)
+		{
+			if ((dp = opendir(av[i])) == NULL)
+			{
+				printf("ft_ls: %s: No such file or directory\n", av[i]);
+				exit(errno);
+			}
+			while ((de = readdir(dp)) != NULL)
+				printf("%s\n", de->d_name);
+			closedir(dp);
+		}
 	}
-	while ((dirent = readdir(dirp)) != NULL)
-		if (strcmp(dirent->d_name, ".") != 0
-			&& strcmp(dirent->d_name, "..") != 0)
-			printf("%s\n", dirent->d_name);
-	closedir(dirp);
 	return (0);
 }
