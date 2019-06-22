f2b927087888093376a89c5ecd4dcecafac1a666
4slan <amalsago@student.42.fr>
2019-06-22 19:21:29 +0200

    removing prototype of ft_listdir() because restarting projetct; adding new fields in s_file structure to store stat about file

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:53:19 by amalsago          #+#    #+#             */
-/*   Updated: 2019/06/09 16:19:13 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/06/21 11:19:03 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,16 @@
 # include <unistd.h>
 # include <uuid/uuid.h>
 # include "../libft/includes/libft.h"
+# include "colors.h"
 
 typedef struct		s_file
 {
+	char			*filename;
+	int				length;
+	struct stat		*stat;
 	struct s_file	*next;
 }					t_file;
 
 int					ft_ls(int ac, char **av);
-int					ft_listdir(const char *path);
 
 #endif
