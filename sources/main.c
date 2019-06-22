9ace23d365497fbce3cc1a0e676ed04791a6d3cf
4slan <amalsago@student.42.fr>
2019-06-22 19:23:45 +0200

    removing SGR tests; thinking how to return 1 if error occurs without quiting program

--- sources/main.c
+++ sources/main.c
@@ -6,56 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:51:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/06/14 16:41:48 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/06/20 16:08:35 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-#include "colors.h"
 
 int			main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
-
-	ft_printf(SGR_BOLD"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FAINT"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_ITALIC"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_UNDERLINE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_SLOWBLINK"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_INVERSE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FG_BLACK"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FG_RED"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FG_GREEN"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FG_YELLOW"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FG_BLUE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FG_MAGENTA"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FG_CYAN"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_FG_WHITE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BG_BLACK"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BG_RED"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BG_GREEN"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BG_YELLOW"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BG_BLUE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BG_MAGENTA"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BG_CYAN"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BG_WHITE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BFG_BLACK"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BFG_RED"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BFG_GREEN"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BFG_YELLOW"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BFG_BLUE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BFG_MAGENTA"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BFG_CYAN"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BFG_WHITE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BBG_BLACK"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BBG_RED"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BBG_GREEN"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BBG_YELLOW"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BBG_BLUE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BBG_MAGENTA"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BBG_CYAN"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	ft_printf(SGR_BBG_WHITE"1234567890 Ди дика хилда !@#$%^&*()_+{}|:\"<>? %s HAM DIY MOGI\n", SGR_NORMAL);
-	//ft_ls(ac - 1, av + 1);
-	return (0);
+	if (ft_ls(ac - 1, av + 1) != 1)
+		return (ERROR_DIGIT); // exits >0 if an error occurs
+	return (0); // exits 0 on success
 }
