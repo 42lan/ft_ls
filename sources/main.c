db69aab6dae419c4a303353b3b373de4b161d298
4slan <amalsago@student.42.fr>
2019-06-23 19:59:51 +0200

    thinking about adding a variable in structure and return its value (0 or 1) if an error occurs

--- sources/main.c
+++ sources/main.c
@@ -6,15 +6,14 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:51:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/06/20 16:08:35 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/06/23 13:57:47 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int			main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	if (ft_ls(ac - 1, av + 1) != 1)
-		return (ERROR_DIGIT); // exits >0 if an error occurs
+	ft_ls(ac - 1, av + 1);
 	return (0); // exits 0 on success
 }
