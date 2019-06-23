7b8d30e8162bf0ec9b8b274bf10b7de190761329
4slan <amalsago@student.42.fr>
2019-06-23 19:57:52 +0200

    creating new structure to hold entry infos; adding prototypes of new functions

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:53:19 by amalsago          #+#    #+#             */
-/*   Updated: 2019/06/21 11:19:03 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/06/23 14:09:19 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 
 #include <errno.h>
 # include <dirent.h>
+# include <grp.h>
 # include <pwd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <sys/xattr.h>
@@ -27,14 +29,29 @@
 # include "../libft/includes/libft.h"
 # include "colors.h"
 
-typedef struct		s_file
+typedef struct		s_dir
 {
-	char			*filename;
+	DIR				*dp;
+	struct dirent	*de;
+	struct s_dir	*next;
+}					t_dir;
+
+typedef struct		s_entry
+{
+	char			*name;
 	int				length;
-	struct stat		*stat;
+	time_t			timestamp;
+	struct stat		stat;
+	struct passwd	*passwd;
+	struct group	*group;
 	struct s_file	*next;
-}					t_file;
+}					t_entry;
 
 int					ft_ls(int ac, char **av);
+void				ft_list_dir(char *entryname);
+int					ft_inspect_entry(t_entry *entry, char *path);
+char				*ft_get_permissions(mode_t mode, int ugo);
+void				ft_modes(char *modes_str, mode_t mode);
+char				ft_type(mode_t mode);
 
 #endif
