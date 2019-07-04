6cfb8f10c14728cea14fa8d279a1db6570cbea97
4slan <amalsago@student.42.fr>
2019-07-04 05:27:00 +0200

    replacing constant value of width by max value that was computate

--- sources/display_long.c
+++ sources/display_long.c
@@ -6,21 +6,19 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 00:52:22 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/04 04:54:30 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 05:20:31 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_long(t_entry *entry)
+void	display_long(t_dir *cd, t_entry *entry)
 {
 	display_mode(entry->stat.st_mode);
-										// need to pass dinamyc width
-	display_nlink(entry->stat.st_nlink, 3);
+	display_nlink(entry->stat.st_nlink, cd->nlink_width);
 	display_ownername(entry->passwd->pw_name);
 	display_groupname(entry->group->gr_name);
-									  // need to pass dinamyc width
-	display_size(entry->stat.st_size, 10);
+	display_size(entry->stat.st_size, cd->size_width);
 	display_mtim(entry->stat.st_mtimespec.tv_sec);
 	display_filename(entry->name);
 }
