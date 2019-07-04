2b4a4ce7ecdee88925165204d88eb7ecaad59d32
4slan <amalsago@student.42.fr>
2019-07-04 05:01:05 +0200

    placing ctime() call here instead in get_mtime()

--- sources/display_mtim.c
+++ sources/display_mtim.c
@@ -6,14 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:27:57 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/04 04:36:50 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 04:50:37 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_mtim(char *mtime_str)
+void		display_mtim(time_t tv_sec)
 {
+	char	*mtime_str;
+
+	mtime_str = ctime(&tv_sec);
 	mtime_str[16] = '\0';
 	ft_printf(" %s", mtime_str + 4);
 }
