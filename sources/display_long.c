78a8af72bdacabb9da75a1501eb8607ecbe46b58
4slan <amalsago@student.42.fr>
2019-07-04 04:57:56 +0200

    replacing getting stuctures in ft_list_dir.c beacase here is only displaying

--- sources/display_long.c
+++ sources/display_long.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 00:52:22 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/04 04:37:02 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 04:54:30 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,13 @@
 
 void	display_long(t_entry *entry)
 {
-	char			*mtime_str;
-
-	get_pwstruct(entry->stat.st_uid, &entry->passwd);
-	get_grstruct(entry->stat.st_gid, &entry->group);
-	mtime_str = get_mtime(entry->stat.st_mtimespec.tv_sec);
-
-	//date = ctime(&entry->timestamp);
-
 	display_mode(entry->stat.st_mode);
-	// need to pass dinamyc width
+										// need to pass dinamyc width
 	display_nlink(entry->stat.st_nlink, 3);
 	display_ownername(entry->passwd->pw_name);
 	display_groupname(entry->group->gr_name);
-	// need to pass dinamyc width
+									  // need to pass dinamyc width
 	display_size(entry->stat.st_size, 10);
-	display_mtim(mtime_str);
+	display_mtim(entry->stat.st_mtimespec.tv_sec);
 	display_filename(entry->name);
 }
