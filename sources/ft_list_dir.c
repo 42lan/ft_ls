e20f8c6a4cadff710bf6a234af6d5e248f5728f0
4slan <amalsago@student.42.fr>
2019-07-04 04:44:08 +0200

    rsaving subdirs in a list

--- sources/ft_list_dir.c
+++ sources/ft_list_dir.c
@@ -1,31 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_dir.c                                      :+:      :+:    :+:   */
+/*   ft_list_c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 13:09:33 by amalsago          #+#    #+#             */
-/*   Updated: 2019/06/23 15:21:20 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 03:25:07 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void			ft_list_dir(char *entryname)
+void				insert_end(t_dir *list, t_dir *new_node)
 {
-	t_dir		dir;
-	t_entry		entry;
+	t_dir			*p;
 
-	if ((dir.dp = opendir(entryname)) != NULL)
+	p = list;
+	while (p->next != NULL)
+		p = p->next;
+	p->next = new_node;
+	new_node->next = NULL;
+}
+
+void				ft_list_dir(DIR *dp, t_dir *cd, char *entryname)
+{
+	t_dir			lst_subdir;
+	t_dir			subdir;
+	struct dirent	*de;
+	t_entry			entry;
+
+	entryname = ft_pathjoin(".", entryname);
+	while ((de = readdir(dp)) != NULL)
 	{
-		while ((dir.de = readdir(dir.dp)) != NULL)
+		entry.name = de->d_name;
+		stat(ft_pathjoin(entryname, de->d_name), &entry.stat);
+		if (entry.stat.st_mode & S_IFDIR) // Если текущий элемент это каталог, то нужно сохранить название каталога, для того, чтобы вызвать на него ft_list_dir() после того, как текущий каталог будет прочтен
 		{
-			ft_inspect_entry(&entry, dir.de->d_name);
-			//ft_printf("%llu\t%s\n", dir.de->d_ino, dir.de->d_name);
+			subdir.name = ft_pathjoin(entryname, de->d_name);
+			insert_end(&lst_subdir, &subdir);
 		}
-		closedir(dir.dp);
+		if (ft_silen(entry.stat.st_nlink, 10) > cd->nlink_width)
+			cd->nlink_width = ft_silen(entry.stat.st_nlink, 10);
+		if (ft_silen(entry.stat.st_size, 10) > cd->size_width)
+			cd->size_width = ft_silen(entry.stat.st_size, 10);
+		stat(entry.name, &entry.stat);
+		display_long(&entry);
+		//ft_inspect_entry(&entry, subdir.name);
 	}
-	else
-		ft_printf("ft_ls: %s: %s\n", entryname, strerror(errno));
 }
