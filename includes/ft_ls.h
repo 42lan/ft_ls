543f3326776b6534fc6ecd58e097aa8fd09ff5ae
4slan <amalsago@student.42.fr>
2019-07-04 04:38:23 +0200

    prototyping new functions; adding new fields to s_dir and s_entry

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:53:19 by amalsago          #+#    #+#             */
-/*   Updated: 2019/06/23 14:09:19 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 04:32:57 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,21 @@
 
 typedef struct		s_dir
 {
-	DIR				*dp;
-	struct dirent	*de;
+	char			*name;			// Current directory name
+	size_t			length;			// Length of current directory
+	char			*parent_name;			// Parent name of current directory
+	char			*fullpath;
+	char			*ownername;
+	char			*groupname;
+	int				nb_files;		// Total number of files in current directory
+	size_t			nlink_width;
+	size_t			size_width;
 	struct s_dir	*next;
 }					t_dir;
 
 typedef struct		s_entry
 {
+	char			*fullpath;
 	char			*name;
 	int				length;
 	time_t			timestamp;
@@ -48,10 +56,39 @@ typedef struct		s_entry
 }					t_entry;
 
 int					ft_ls(int ac, char **av);
-void				ft_list_dir(char *entryname);
+void				parse_options(char *first_arg, int *options);
+void				usage(char c);
+
+
+void				ft_list_dir(DIR *dp, t_dir *cd, char *entryname);
 int					ft_inspect_entry(t_entry *entry, char *path);
-char				*ft_get_permissions(mode_t mode, int ugo);
-void				ft_modes(char *modes_str, mode_t mode);
 char				ft_type(mode_t mode);
+void				determine_max_width(t_dir *directory, t_entry *entry);
+void				initialize_directory(t_dir *directory);
+void				parse_entry(char *entryname, t_dir *cd_struct);
+char				*ft_pathjoin(const char *parent, const char *child);
+
+char				*get_permissions(mode_t mode, int ugo);
+void				modecat(char *str, mode_t st_mode);
+
+
+
+void	get_pwstruct(uid_t st_uid, struct passwd **passwd);
+void	get_grstruct(gid_t st_gid, struct group **group);
+char	*get_mtime(time_t tv_sec);
+/*
+** OUTPUT
+*/
+
+void	display_default();
+void	display_long(t_entry *entry);
+
+void	display_mode(mode_t st_mode);
+void	display_nlink(nlink_t st_nlink, int width);
+void	display_ownername(char *pw_name);
+void	display_groupname(char *gr_name);
+void	display_size(off_t st_size, int width);
+void	display_mtim(char *mtime_str);
+void	display_filename(char *filename);
 
 #endif
