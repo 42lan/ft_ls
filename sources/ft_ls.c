bbbe2a76205dcb4a583324618feff656f35b573f
4slan <amalsago@student.42.fr>
2019-07-04 04:45:14 +0200

    handeling call without args or with arguments for folder and regfile

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,21 +6,53 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 11:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/06/23 13:58:20 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 01:50:55 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int			ft_ls(int ac, char **av)
+int					ft_ls(int ac, char **av)
 {
-	int		i;
+	int				i;
+	DIR				*dp;
+	t_dir			cd;
+	t_entry			entry;
+	int				options;
 
 	i = -1;
+	dp = NULL;
+	options = 0;
 	if (ac == 0)
-		ft_list_dir(".");
+	{
+		cd.parent_name = NULL;
+		dp = opendir(".");
+		ft_list_dir(dp, &cd, ".");
+		closedir(dp);
+	}
 	else
+	{
+		if (av[0][0] == '-' && av[0][1] != '\0')
+		{
+			parse_options(av[0], &options);
+			++i;
+		}
 		while (++i < ac)
-			ft_list_dir(av[i]);
-	return (1); // return 1 to main as success
+		{
+			if ((dp = opendir(av[i])) != NULL)
+			{
+				ft_list_dir(dp, &cd, av[i]);
+				determine_max_width(&cd, &entry);
+				closedir(dp);
+			}
+			else
+			{
+				stat(entry.name, &entry.stat);
+				entry.name = av[i];
+				if (ft_inspect_entry(&entry, av[i]) != 1)
+					ft_printf("ft_ls: %s: %s\n", av[i], strerror(errno));
+			}
+		}
+	}
+	return (1);
 }
