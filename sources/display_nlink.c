b8669014663f031bd060e49eea607b15fd12fa07
4slan <amalsago@student.42.fr>
2019-07-04 05:29:21 +0200

    putting a space before displaying ownername

--- sources/display_nlink.c
+++ sources/display_nlink.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:05:19 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/04 03:08:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 05:12:18 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	display_nlink(nlink_t st_nlink, int width)
 {
-	ft_printf("%*d", width, st_nlink);
+	ft_printf("%*d ", width, st_nlink);
 }
