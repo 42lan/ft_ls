ff44fe542a04d9b62158997d06df5bbc5825875d
4slan <amalsago@student.42.fr>
2019-07-04 05:30:06 +0200

    space before pw_name is putted by display_nlink(); putting two spaces before displaying groupname

--- sources/display_ownername.c
+++ sources/display_ownername.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:10:30 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/04 03:11:51 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 05:19:11 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	display_ownername(char *pw_name)
 {
-	ft_printf(" %s ", pw_name);
+	ft_printf("%s  ", pw_name);
 }
