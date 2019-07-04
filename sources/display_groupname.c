014830b443d1fa0e992a786803cca47b6cb1079c
4slan <amalsago@student.42.fr>
2019-07-04 07:41:39 +0200

    adding new parameter to set width while displaying groupname

--- sources/display_groupname.c
+++ sources/display_groupname.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:13:33 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/04 05:19:24 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/04 07:23:45 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_groupname(char *gr_name)
+void	display_groupname(char *gr_name, size_t width)
 {
-	ft_printf("%s  ", gr_name);
+	ft_printf("%-*s  ", width, gr_name);
 }
