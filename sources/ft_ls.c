c0ce916571f1631e2a8dd122a1a0b0fc479a9ce3
4slan <amalsago@student.42.fr>
2019-07-13 09:59:59 +0200

    using functions instead writing all in one

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 11:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/04 07:25:10 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/12 03:30:40 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,58 +15,20 @@
 int					ft_ls(int ac, char **av)
 {
 	int				i;
-	DIR				*dp;
-	t_dir			cd;
-	t_entry			entry;
 	int				options;
-	t_list			sdl;	// sub directory list
-	struct dirent	*de;
+	int				opt_bits;
+	struct dirent	*dirent;
+	t_dir			current_dir;
 
-
-	i = -1;
-	dp = NULL;
-	options = 0;
+	dirent = NULL;
+	opt_bits = 0;
+	// parse_options() retourne l'index de l'agument qui n'est pas une options
+	options = parse_options(ac, av, &opt_bits);
+	i = options; // je me la valeur de ce index dans la variable
 	if (ac == 0)
 	{
-		cd.parent_name = NULL;
-		dp = opendir(".");
-		ft_list_dir(dp, &cd, ".", &sdl);
-		closedir(dp);
+		browse_dir(".", dirent, &current_dir);
 	}
-	else
-	{
-		if (av[0][0] == '-' && av[0][1] != '\0')
-		{
-			parse_options(av[0], &options);
-			++i;
-		}
-		while (++i < ac)
-		{
-			if ((dp = opendir(av[i])) != NULL)
-			{
-				ft_list_dir(dp, &cd, av[i], &sdl);
-				determine_max_width(&cd, &entry);
-				closedir(dp);
-				dp = opendir(av[i]);
-				while ((de = readdir(dp)) != NULL)
-				{
-					entry.name = de->d_name;
-					stat(ft_pathjoin(av[i], de->d_name), &entry.stat);
-					stat(entry.name, &entry.stat);
-					get_pwstruct(entry.stat.st_uid, &entry.passwd);
-					get_grstruct(entry.stat.st_gid, &entry.group);
-					display_long(&cd, &entry);
-				}
-				closedir(dp);
-			}
-			else
-			{
-				stat(entry.name, &entry.stat);
-				entry.name = av[i];
-				if (ft_inspect_entry(&entry, av[i]) != 1)
-					ft_printf("ft_ls: %s: %s\n", av[i], strerror(errno));
-			}
-		}
-	}
+	// while (i < ac){if ((dp = opendir(av[i])) != NULL){browse_dir(dp, dirent);}closedir(dp);i++;}
 	return (1);
 }
