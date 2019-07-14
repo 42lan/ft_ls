2e5954ac93df6435e0d0ceb7c58b07860c44f8d5
4slan <amalsago@student.42.fr>
2019-07-14 07:46:36 +0200

    refactoring

--- sources/output/display_long.c
+++ sources/output/display_long.c
@@ -5,20 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/02 00:52:22 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/13 05:49:50 by amalsago         ###   ########.fr       */
+/*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
+/*   Updated: 2019/07/14 07:15:06 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_long(t_dir *cd, t_file *entry)
+void	display_long(t_dir *current_dir, t_file *list)
 {
-	display_mode(entry->stat.st_mode);
-	display_nlink(entry->stat.st_nlink, cd->nlink_wmax);
-	display_ownername(entry->ownername, cd->ownername_wmax);
-	display_groupname(entry->groupname, cd->groupname_wmax);
-	display_size(entry->stat.st_size, cd->size_wmax);
-	display_mtim(entry->stat.st_mtimespec.tv_sec);
-	display_filename(entry->name);
+	list = list->next;
+	while (list != NULL)
+	{
+		display_mode(list->stat.st_mode);
+		display_nlink(list->stat.st_nlink, current_dir->nlink_wmax);
+		display_ownername(list->ownername, 0);
+		display_groupname(list->groupname, 0);
+		display_size(list->stat.st_size, current_dir->size_wmax);
+		display_mtim(list->stat.st_mtimespec.tv_sec);
+		display_filename(list->name, 1);
+		ft_putstr("\n");
+		list = list->next;
+	}
 }
