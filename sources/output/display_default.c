f2d27295d78774fea4ed468eed3cc3ebc8d73798
4slan <amalsago@student.42.fr>
2019-07-14 07:41:21 +0200

    definition of function which is called when ft_ls is called without options

--- sources/output/display_default.c
+++ sources/output/display_default.c
@@ -6,13 +6,19 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 03:21:49 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/07 03:00:17 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/14 07:17:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_default()
+void	display_default(t_dir *current_dir, t_file *list)
 {
-	display_filename(NULL);
+	printf("%s %zu\n", list->name, current_dir->namlen_wmax);
+	list = list->next;
+	while (list != NULL)
+	{
+		//display_filename(list->name, -current_dir->namlen_wmax);
+		list = list->next;
+	}
 }
