4a4256eb8ca11a9b933d95c69d03fc28ee41d855
4slan <amalsago@student.42.fr>
2019-07-17 17:41:18 +0200

    head of chained list is placed in t_dir

--- sources/output/display_long.c
+++ sources/output/display_long.c
@@ -6,25 +6,24 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/14 07:15:06 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/17 15:01:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_long(t_dir *current_dir, t_file *list)
+void	display_long(t_dir *current_dir)
 {
-	list = list->next;
-	while (list != NULL)
+	while (current_dir->file_head != NULL)
 	{
-		display_mode(list->stat.st_mode);
-		display_nlink(list->stat.st_nlink, current_dir->nlink_wmax);
-		display_ownername(list->ownername, 0);
-		display_groupname(list->groupname, 0);
-		display_size(list->stat.st_size, current_dir->size_wmax);
-		display_mtim(list->stat.st_mtimespec.tv_sec);
-		display_filename(list->name, 1);
+		display_mode(current_dir->file_head->stat.st_mode);
+		display_nlink(current_dir->file_head->stat.st_nlink, current_dir->nlink_wmax);
+		display_ownername(current_dir->file_head->ownername, current_dir->ownername_wmax);
+		display_groupname(current_dir->file_head->groupname, current_dir->groupname_wmax);
+		display_size(current_dir->file_head->stat.st_size, current_dir->size_wmax);
+		display_mtim(current_dir->file_head->stat.st_mtimespec.tv_sec);
+		display_filename(current_dir->file_head->name, 1);
 		ft_putstr("\n");
-		list = list->next;
+		current_dir->file_head = current_dir->file_head->next;
 	}
 }
