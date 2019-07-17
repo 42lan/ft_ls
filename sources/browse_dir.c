7a4ea0b80b6698b0a7edf0d4dd78d68932fef112
4slan <amalsago@student.42.fr>
2019-07-17 17:38:29 +0200

    refactoring by using external functions

--- sources/browse_dir.c
+++ sources/browse_dir.c
@@ -6,54 +6,45 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 21:42:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/14 07:15:20 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/17 17:37:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-
-void				browse_dir(const char *path, struct dirent *dirent, t_dir *current_dir)
+t_dir				*browse_dir(const char *path)
 {
 	DIR				*dp;
+	struct dirent	*dirent;
+	t_dir			*current_dir;
 	t_file			*file;
-	t_file			*list;
-	t_list			subdir_list;
-	struct passwd	*passwd;
-	struct group	*group;
+	t_sdir			*sdir;
 
-	list = new_file();
-	if ((dp = opendir(path)) != NULL)
+	if ((dp = opendir(path)) == NULL)					// Trying to open given path
+		return (NULL);
+	current_dir = initialize_directory();
+	current_dir->name = ft_strdup(path);
+	sdir = (t_sdir *)malloc(sizeof(t_sdir));			// Allocation memory for subdirectories. Need to allocate
+	while ((dirent = readdir(dp)) != NULL)				// Reading directory entry by entry
 	{
-		initialize_directory(current_dir);
-		ft_strcpy(current_dir->name, path);
-		while ((dirent = readdir(dp)) != NULL)
-		{
-			if (is_hidden(dirent->d_name))
-				continue ;
-			file = new_file();
-
-			stat(dirent->d_name, &file->stat);
-			if ((passwd = get_pwstruct(file->stat.st_uid)) == NULL)
-				return ;
-			if ((group = get_grstruct(file->stat.st_gid)) == NULL)
-				return ;
-			file->name = dirent->d_name;
-			file->namlen = dirent->d_namlen;
-			file->ownername = passwd->pw_name;
-			file->groupname = group->gr_name;
-			++(current_dir->nb_files);
-			determine_wmax(dirent, file, current_dir);
-			if (is_directory(file->stat.st_mode)) // Checking for subdirectories
-			{
-				// Forming relative path and saving it
-				subdir_list.content = form_path(current_dir->name, dirent->d_name);
-				subdir_list.next = NULL;
-			}
-			add_file(list, file);
-		}
+		++(current_dir->nb_files);						// Counting number of files including hidden + . and ..
+		if (is_hidden(dirent->d_name))					// Checking for hidden files
+			continue ;									// Skipping hiddent files
+		file = new_file();								// !!! Need to check allocation
+		if (current_dir->file_head == NULL)				// Checking if file_head pointer is NULL
+			current_dir->file_head = file;				// If file_head pointer pointed no NULL so now it points to first file
+		fill_file_struct(file, dirent);					// Filling file structure
+		determine_wmax(dirent, file, current_dir);
+		check_subdir(file, dirent, current_dir, sdir);
+		push_end(current_dir->file_head, file);			// Appending new node to file list
 	}
-	display_default(current_dir, list);
-	//display_long(current_dir, list);
 	closedir(dp);
+	/*
+	while (current_dir->sdir_head != NULL)
+	{
+		browse_dir(current_dir->sdir_head->path, current_dir);
+		current_dir->sdir_head = current_dir->sdir_head->next;
+	}
+	*/
+	return (current_dir);
 }
