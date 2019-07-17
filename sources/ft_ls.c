2af9021023d3dad54c5719d5a160ffd0ecf2a624
4slan <amalsago@student.42.fr>
2019-07-17 17:39:05 +0200

    refactoring call of functions

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 11:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/12 03:30:40 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/17 15:00:30 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,15 @@ int					ft_ls(int ac, char **av)
 	int				i;
 	int				options;
 	int				opt_bits;
-	struct dirent	*dirent;
-	t_dir			current_dir;
+	t_dir			*current_dir;
 
-	dirent = NULL;
 	opt_bits = 0;
-	// parse_options() retourne l'index de l'agument qui n'est pas une options
-	options = parse_options(ac, av, &opt_bits);
+	options = parse_options(ac, av, &opt_bits); // parse_options() retourne l'index de l'agument qui n'est pas une options
 	i = options; // je me la valeur de ce index dans la variable
 	if (ac == 0)
 	{
-		browse_dir(".", dirent, &current_dir);
+		current_dir = browse_dir(".");
+		display_long(current_dir);
 	}
 	// while (i < ac){if ((dp = opendir(av[i])) != NULL){browse_dir(dp, dirent);}closedir(dp);i++;}
 	return (1);
