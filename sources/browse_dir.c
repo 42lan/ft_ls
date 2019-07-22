6a2b934b403ee2bbb72a173b6ed8c7fd88b521a7
4slan <amalsago@student.42.fr>
2019-07-22 13:12:25 +0200

    printing sdir list for test

--- sources/browse_dir.c
+++ sources/browse_dir.c
@@ -6,11 +6,27 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 21:42:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/17 17:37:28 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/22 12:52:10 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+void				print_list(t_list *list)
+{
+	while (list != NULL)
+	{
+		if (list->next == NULL)
+			ft_printf("%s -> NULL\n", list->content);
+		else
+		{
+			//ft_printf("%s -> ", list->content);
+			write(1, list->content, list->content_size);
+			write(1, " -> ", 4);
+		}
+		list = list->next;
+	}
+}
 
 t_dir				*browse_dir(const char *path)
 {
@@ -18,13 +34,11 @@ t_dir				*browse_dir(const char *path)
 	struct dirent	*dirent;
 	t_dir			*current_dir;
 	t_file			*file;
-	t_sdir			*sdir;
 
 	if ((dp = opendir(path)) == NULL)					// Trying to open given path
 		return (NULL);
 	current_dir = initialize_directory();
 	current_dir->name = ft_strdup(path);
-	sdir = (t_sdir *)malloc(sizeof(t_sdir));			// Allocation memory for subdirectories. Need to allocate
 	while ((dirent = readdir(dp)) != NULL)				// Reading directory entry by entry
 	{
 		++(current_dir->nb_files);						// Counting number of files including hidden + . and ..
@@ -35,16 +49,11 @@ t_dir				*browse_dir(const char *path)
 			current_dir->file_head = file;				// If file_head pointer pointed no NULL so now it points to first file
 		fill_file_struct(file, dirent);					// Filling file structure
 		determine_wmax(dirent, file, current_dir);
-		check_subdir(file, dirent, current_dir, sdir);
+		if (check_subdir(file, dirent, current_dir))		// Checking if actual file is a directory
+			print_list(current_dir->sdir_head);
 		push_end(current_dir->file_head, file);			// Appending new node to file list
 	}
 	closedir(dp);
-	/*
-	while (current_dir->sdir_head != NULL)
-	{
-		browse_dir(current_dir->sdir_head->path, current_dir);
-		current_dir->sdir_head = current_dir->sdir_head->next;
-	}
-	*/
+	//print_list(current_dir->sdir_head);
 	return (current_dir);
 }
