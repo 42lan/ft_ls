c91845dc9820fca77712050af809faf86c60e74b
4slan <amalsago@student.42.fr>
2019-07-24 19:02:21 +0200

    stat is a pointer in s_file

--- sources/wmax/determine_size_wmax.c
+++ sources/wmax/determine_size_wmax.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 03:59:04 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/14 03:59:21 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/24 12:27:52 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void		determine_size_wmax(t_file *file, t_dir *current_dir)
 {
 	size_t	size_w;
 
-	size_w = ft_uilen(file->stat.st_size, 10);
+	size_w = ft_uilen(file->stat->st_size, 10);
 	if (size_w > current_dir->size_wmax)
 		current_dir->size_wmax = size_w;
 }
