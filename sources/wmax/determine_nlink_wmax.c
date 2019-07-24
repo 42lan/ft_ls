3b9ffc80fde1e5c0437214d566ec368c27018a18
4slan <amalsago@student.42.fr>
2019-07-24 19:02:07 +0200

    sta is a poiner in s_file

--- sources/wmax/determine_nlink_wmax.c
+++ sources/wmax/determine_nlink_wmax.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 03:59:45 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/14 03:59:55 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/24 12:27:42 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void		determine_nlink_wmax(t_file *file, t_dir *current_dir)
 {
 	size_t	nlink_w;
 
-	nlink_w = ft_uilen(file->stat.st_nlink, 10);
+	nlink_w = ft_uilen(file->stat->st_nlink, 10);
 	if (nlink_w > current_dir->nlink_wmax)
 		current_dir->nlink_wmax = nlink_w;
 }
