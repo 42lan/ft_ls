b3d7597c2525e1bc651d0a034b2d6b4659cb22e3
4slan <amalsago@student.42.fr>
2019-07-24 19:14:15 +0200

    need to review to exit properly function

--- sources/fill_file_struct.c
+++ sources/fill_file_struct.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 10:28:56 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/24 12:27:10 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/24 19:05:59 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,11 @@ void	fill_file_struct(t_file *file, struct dirent *dirent)
 {
 	struct passwd	*passwd;
 	struct group	*group;
-	char			*relative_name;
 
-	relative_name = form_path(file->dirname, dirent->d_name);
-	//ft_printf("<< %s\n", relative_name);
-	file->stat = get_stat(relative_name);
-	if ((passwd = get_pwstruct(file->stat->st_uid)) == NULL)
-		return ;
-	if ((group = get_grstruct(file->stat->st_gid)) == NULL)
-		return ;
+	if ((passwd = get_pwstruct(file->stat->st_uid)) == NULL) //// Need to quit properly
+		return ; //////////////////////////////////////////////// Need to quit properly
+	if ((group = get_grstruct(file->stat->st_gid)) == NULL) ///// Need to quit properly
+		return ; //////////////////////////////////////////////// Need to quit properly
 	file->name = dirent->d_name;
 	file->namlen = dirent->d_namlen;
 	file->ownername = passwd->pw_name;
