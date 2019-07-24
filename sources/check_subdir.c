098d1e0bc77f7e5d40811dc3a540ea26cd156c16
4slan <amalsago@student.42.fr>
2019-07-24 19:49:33 +0200

    relpath and is done in browse_dir.c

--- sources/check_subdir.c
+++ sources/check_subdir.c
@@ -6,28 +6,25 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 10:37:26 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/24 14:33:10 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/24 19:43:13 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int			check_subdir(t_file *file, struct dirent *dirent, t_dir *current_dir)
+int			check_subdir(t_file *file, t_dir *current_dir)
 {
-	char	*path;
 	t_list	*new_sdir;
 
 	if (is_directory(file->stat->st_mode))
 	{
-		// Forming a relative path for a subdirectory
-		path = form_path(current_dir->name, dirent->d_name);
 		// If sdir_head is empty, so the first node of a link list is the first readed subdir
 		if (current_dir->sdir_head == NULL)
-			current_dir->sdir_head = ft_lstnew(path, ft_strlen(path));
+			current_dir->sdir_head = ft_lstnew(file->relpath, ft_strlen(file->relpath));
 		// Else, sdir_head has a node, so append linked list with new subdir
 		else
 		{
-			new_sdir = ft_lstnew(path, ft_strlen(path));
+			new_sdir = ft_lstnew(file->relpath, ft_strlen(file->relpath));
 			ft_lstadd_end(&current_dir->sdir_head, new_sdir);
 		}
 		return (1);
