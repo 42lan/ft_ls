551f8c720ec6af14bd6fedc3a19f6b8b8ca11493
4slan <amalsago@student.42.fr>
2019-07-27 19:20:36 +0200

    defining global array of options and trying to use it

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,27 +6,40 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 11:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/25 15:58:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/27 18:49:19 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int					ft_ls(int ac, char **av)
+t_argp			g_argp[] =
 {
-	int				i;
-	//int				options;
-	//int				opt_bits;
-	t_file			*file;
+	{'1', 0, "List one file per line"},
+	{'l', 0, "List in long format"},
+	{'R', 0, "Recursively list subdirectories encountered"},
+	{'a', 0, "List files whose names begin with a dot (.)"},
+	{'r', 0, "Reverse the order of the sort"},
+	{'t', 0, "Sort by time modified (most recently modified first)"},
+	{'-', 0, "End of options"},
+	{0, 0, NULL}
+};
+
+int				ft_ls(int ac, char **av)
+{
+	int			i;
+	t_file		*file;
 
 	i = -1;
-	//opt_bits = 0;
-	//options = parse_options(ac, av, &opt_bits); // parse_options() retourne l'index de l'agument qui n'est pas une options
 	if (ac == 0)
 		browse_dir(".");
 	else
 	{
-		//parse_options();
+		parse_argp(ac, av, g_argp);
+		if (g_argp[1].active == 1)
+		{
+			ft_printf("OKOKOK\n");
+			return (1);
+		}
 		while (++i < ac)
 		{
 			file = new_file();
