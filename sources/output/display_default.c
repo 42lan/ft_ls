e7a1fe18f5f959539357b9ffa4b244670c26667f
4slan <amalsago@student.42.fr>
2019-07-30 08:37:54 +0200

    getting terminal size, need to think how i will use it

--- sources/output/display_default.c
+++ sources/output/display_default.c
@@ -6,18 +6,26 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 03:21:49 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/17 17:51:20 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/07/30 08:30:34 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_default(t_dir *current_dir)
+void					display_default(t_dir *current_dir)
 {
+	struct winsize		w;
+
+	ioctl(0, TIOCGWINSZ, &w);
 	while (current_dir->file_head != NULL)
 	{
-		display_filename(current_dir->file_head->name, 0);
-		ft_putendl("");
+		if (current_dir->file_head->next == NULL)
+		{
+			display_filename(current_dir->file_head->name, 0);
+			ft_putchar('\n');
+		}
+		else
+			display_filename(current_dir->file_head->name, -(current_dir->namlen_wmax + 4));
 		current_dir->file_head = current_dir->file_head->next;
 	}
 }
