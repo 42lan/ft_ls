b676930b11bd8ac53e588df35a91c20d3c989ef4
4slan <amalsago@student.42.fr>
2019-07-30 08:35:41 +0200

    correcting value of g_argp element

--- sources/output/display.c
+++ sources/output/display.c
@@ -1,10 +1,10 @@
 #include "ft_ls.h"
 
-extern t_argp		g_argp[];
+extern t_argp	g_argp[];
 
-void	display(t_dir *current_dir)
+void			display(t_dir *current_dir)
 {
-	if (g_argp[1].active == 1) // If -l is specified
+	if (g_argp[0].active == 1) // If -l is specified
 		display_long(current_dir);
 	else
 		display_default(current_dir);
