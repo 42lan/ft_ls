80e389af0b26bfe695f248170a26530730bf9f94
4slan <amalsago@student.42.fr>
2019-08-02 22:14:38 +0200

    resetting value of k for each char of av to loop through g_argp

--- sources/parsing/parse_argp.c
+++ sources/parsing/parse_argp.c
@@ -6,23 +6,26 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 18:31:16 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/27 18:46:33 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/02 20:44:35 by aslan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-extern t_argp			g_argp[];
+extern t_argp	g_argp[];
 
-void		parse_argp(char *av)
+void			parse_argp(char *av)
 {
-	int		i;
-	int		k;
+	int			i;
+	int			k;
 
 	i = 0; // Start with second (index 1) character
 	k = -1;
 	while (av[++i] != '\0')
+	{
+		k = -1;
 		while (g_argp[++k].sign != 0)
 			if (g_argp[k].sign == av[i])
 				g_argp[k].active = 1;
+	}
 }
