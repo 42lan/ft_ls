17032a7977d21afde8af550566ac5e0e74f55648
4slan <amalsago@student.42.fr>
2019-08-05 11:35:09 +0200

    setting values of structure's fields to 0/NULL

--- sources/new_file.c
+++ sources/new_file.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/16 15:29:55 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/16 15:30:50 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/04 15:48:35 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ t_file		*new_file(void)
 {
 	t_file	*new_file;
 
-	if (!(new_file = (t_file *)malloc(sizeof(t_file))))
+	if (!(new_file = (t_file *)ft_memalloc(sizeof(t_file))))
 		return (NULL);
+	new_file->name = NULL;
+	new_file->relpath = NULL;
+	new_file->namlen = 0;
+	new_file->stat = NULL;
+	new_file->ownername = NULL;
+	new_file->groupname = NULL;
 	new_file->next = NULL;
 	return (new_file);
 }
