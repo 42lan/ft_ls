ccf2293e83d82405e3cd1129ce9849de0191dfaa
4slan <amalsago@student.42.fr>
2019-08-05 11:34:24 +0200

    adding +1 to size because each string need to be ended by '\0'

--- sources/check_subdir.c
+++ sources/check_subdir.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 10:37:26 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/24 19:43:13 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/05 11:33:18 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int			check_subdir(t_file *file, t_dir *current_dir)
 	{
 		// If sdir_head is empty, so the first node of a link list is the first readed subdir
 		if (current_dir->sdir_head == NULL)
-			current_dir->sdir_head = ft_lstnew(file->relpath, ft_strlen(file->relpath));
+			current_dir->sdir_head = ft_lstnew(file->relpath, ft_strlen(file->relpath) + 1); // abaisago: +1 pour '\0'
 		// Else, sdir_head has a node, so append linked list with new subdir
 		else
 		{
-			new_sdir = ft_lstnew(file->relpath, ft_strlen(file->relpath));
+			new_sdir = ft_lstnew(file->relpath, ft_strlen(file->relpath) + 1); // abaisago: +1 pour '\0'
 			ft_lstadd_end(&current_dir->sdir_head, new_sdir);
 		}
 		return (1);
