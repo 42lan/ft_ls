8892e0a9237d2847518f2260ff2722a4a4b7b571
4slan <amalsago@student.42.fr>
2019-08-05 10:49:31 +0200

    refactoring code to avoid cast in each interation of while loop

--- libft/sources/memory/ft_memcpy.c
+++ libft/sources/memory/ft_memcpy.c
@@ -6,18 +6,22 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 15:53:20 by amalsago          #+#    #+#             */
-/*   Updated: 2019/04/29 10:07:27 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/05 10:48:14 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		*ft_memcpy(void *dst, const void *src, size_t n)
+void			*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
+	size_t		i;
+	const char	*src_str;
+	char		*dst_str;
 
 	i = -1;
+	src_str = (const char *)src;
+	dst_str = (char *)dst;
 	while (++i < n)
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-	return (dst);
+		dst_str[i] = src_str[i];
+	return (dst_str);
 }
