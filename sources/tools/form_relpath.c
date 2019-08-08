f0cd05af334b18a42839329d7a951ad844598b4d
4slan <amalsago@student.42.fr>
2019-08-08 15:01:53 +0200

    to avoid name // when argv is '/'

--- sources/tools/form_relpath.c
+++ sources/tools/form_relpath.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 15:59:17 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/24 19:32:20 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/08 13:44:24 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ char		*form_relpath(const char *dirname, char const *basename)
 	if (!(path = ft_strnew(ft_strlen(dirname) + 1 + ft_strlen(basename))))
 		return (NULL);
 	path = ft_strcpy(path, dirname);
-	path = ft_strcat(path, "/");
+	if (!ft_strequ(dirname, "/"))
+		path = ft_strcat(path, "/");
 	path = ft_strcat(path, basename);
 	return (path);
 }
