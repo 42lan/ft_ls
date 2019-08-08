d54d4c65a60c55d1e68b9aaa286fda1abb63dd33
4slan <amalsago@student.42.fr>
2019-08-08 13:28:28 +0200

    using new explicit name of defined marcos

--- sources/output/display.c
+++ sources/output/display.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 21:07:44 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/30 09:08:04 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/08 13:24:57 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ extern t_argp	g_argp[];
 
 void			display(t_dir *current_dir)
 {
-	if (g_argp[0].active == 1) // If -l is specified
+	if (g_argp[LONG_FORMAT].active == 1)
 		display_long(current_dir);
 	else
 		display_default(current_dir);
