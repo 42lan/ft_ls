e772522e31db4321c98a0c74b4d1bf50bf39b4f7
4slan <amalsago@student.42.fr>
2019-08-08 13:28:04 +0200

    refactoring get_stat() to check returned value

--- sources/gets/get_stat.c
+++ sources/gets/get_stat.c
@@ -6,20 +6,22 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/24 12:16:53 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/24 18:57:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/08 11:50:39 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-struct stat			*get_stat(const char *path)
+int					get_stat(const char *path, t_file *file)
 {
-	struct stat		*file_stat;
-
 	if (path == NULL)
-		return (NULL);
-	if (!(file_stat = (struct stat *)ft_memalloc(sizeof(struct stat))))
-		return (NULL);
-	stat(path, file_stat);
-	return (file_stat);
+		return (0);
+	if (!(file->stat = (struct stat *)ft_memalloc(sizeof(struct stat))))
+		return (0);
+	if (stat(path, file->stat) == -1)
+	{
+		ft_printf("get_stat(): %s\n", strerror(errno));
+		return (0);
+	}
+	return (1);
 }
