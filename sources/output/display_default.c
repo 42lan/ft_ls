f050b115b58b3887b800b89340b6e87b8dc2de1d
4slan <amalsago@student.42.fr>
2019-08-12 16:18:29 +0200

    reduce name to stay in 80col; adding new arg to display_filename()

--- sources/output/display_default.c
+++ sources/output/display_default.c
@@ -6,30 +6,24 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 03:21:49 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/06 09:56:37 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/12 15:48:45 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void					display_default(t_dir *current_dir)
+void					display_default(t_dir *dir)
 {
 	struct winsize		w;
 
 	ioctl(STDIN_FILENO, TIOCGWINSZ, &w);
-	while (current_dir->file_head != NULL)
+	while (dir->file_head != NULL)
 	{
 		//place_cursor(x, y);
-		if (current_dir->file_head->next == NULL)
-		{
-			display_filename(current_dir->file_head->name, 0);
-			ft_putchar('\n');
-		}
+		if (dir->file_head->next == NULL)
+			display_filename(dir->file_head->name, dir->file_head->namlen, 0);
 		else
-		{
-			display_filename(current_dir->file_head->name, 0);
-			ft_putchar('\n');
-		}
-		current_dir->file_head = current_dir->file_head->next;
+			display_filename(dir->file_head->name, dir->file_head->namlen, 0);
+		dir->file_head = dir->file_head->next;
 	}
 }
