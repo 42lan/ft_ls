d43dd1ffa69cc8b012786037d452cd3b961d882a
4slan <amalsago@student.42.fr>
2019-08-12 16:20:36 +0200

    adding new arg to display_filename() to fix spacement before filename

--- sources/output/display_filename.c
+++ sources/output/display_filename.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:18:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/14 07:11:59 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/12 15:45:59 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_filename(char *filename, size_t width)
+void	display_filename(char *filename, int namlen, int width)
 {
-	ft_printf("%*s", width, filename);
+	ft_printf("%*s\n", namlen + width, filename);
 }
