1a518c9f5a0f883a3990f1fdd139aa451551076e
4slan <amalsago@student.42.fr>
2019-08-12 16:21:15 +0200

    renaming current_dir into dir to stay in 80 colums

--- sources/output/display_long.c
+++ sources/output/display_long.c
@@ -6,26 +6,25 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/05 11:35:47 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/12 15:43:26 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_long(t_dir *current_dir)
+void	display_long(t_dir *dir)
 {
-	if (current_dir->file_head->next != NULL)
-		display_total(current_dir->total_blocks);
-	while (current_dir->file_head != NULL)
+	if (dir->file_head->next != NULL)
+		display_total(dir->total_blocks);
+	while (dir->file_head != NULL)
 	{
-		display_mode(current_dir->file_head->stat->st_mode);
-		display_nlink(current_dir->file_head->stat->st_nlink, current_dir->nlink_wmax);
-		display_ownername(current_dir->file_head->ownername, current_dir->ownername_wmax);
-		display_groupname(current_dir->file_head->groupname, current_dir->groupname_wmax);
-		display_size(current_dir->file_head->stat->st_size, current_dir->size_wmax);
-		display_mtim(current_dir->file_head->stat->st_mtimespec.tv_sec);
-		display_filename(current_dir->file_head->name, 1);
-		ft_putchar('\n');
-		current_dir->file_head = current_dir->file_head->next;
+		display_mode(dir->file_head->stat->st_mode);
+		display_nlink(dir->file_head->stat->st_nlink, dir->nlink_wmax);
+		display_ownername(dir->file_head->ownername, dir->ownername_wmax);
+		display_groupname(dir->file_head->groupname, dir->groupname_wmax);
+		display_size(dir->file_head->stat->st_size, dir->size_wmax);
+		display_mtim(dir->file_head->stat->st_mtimespec.tv_sec);
+		display_filename(dir->file_head->name, dir->file_head->namlen, 1);
+		dir->file_head = dir->file_head->next;
 	}
 }
