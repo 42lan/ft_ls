442b24b88eaa78487202951e476e2568f9336d9f
4slan <amalsago@student.42.fr>
2019-08-19 18:35:33 +0200

    calling lstat to get follow symlink; adding error message

--- sources/gets/get_stat.c
+++ sources/gets/get_stat.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/24 12:16:53 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/08 11:50:39 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/19 17:14:37 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int					get_stat(const char *path, t_file *file)
 		return (0);
 	if (!(file->stat = (struct stat *)ft_memalloc(sizeof(struct stat))))
 		return (0);
-	if (stat(path, file->stat) == -1)
+	if (lstat(path, file->stat) == -1)
 	{
-		ft_printf("get_stat(): %s\n", strerror(errno));
+		ft_printf("lstat(%s): %s\n", path, strerror(errno));
 		return (0);
 	}
 	return (1);
