7b39d730afacb629755ca1fc7faec60b6465baea
4slan <amalsago@student.42.fr>
2019-08-19 18:27:16 +0200

    display_filename() need to access to same fields of t_file

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:53:19 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/12 15:41:54 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/19 17:09:32 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,7 +128,7 @@ void	display_ownername(char *ownername, size_t width);
 void	display_groupname(char *groupname, size_t width);
 void	display_size(off_t st_size, size_t width);
 void	display_mtim(time_t tv_sec);
-void	display_filename(char *filename, int namlen, int width);
+void	display_filename(t_file *file, int width);
 
 /* TOOLS */
 void	modecat(char *str, mode_t st_mode);
