a7811bf81cb807a83f0c79d3cf01555dc2da7f74
4slan <amalsago@student.42.fr>
2019-08-19 18:48:46 +0200

    passing t_file to display_filename() as it prototyped

--- sources/output/display_long.c
+++ sources/output/display_long.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/12 15:43:26 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/19 17:28:05 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@ void	display_long(t_dir *dir)
 	while (dir->file_head != NULL)
 	{
 		display_mode(dir->file_head->stat->st_mode);
+		//display_acl();
 		display_nlink(dir->file_head->stat->st_nlink, dir->nlink_wmax);
 		display_ownername(dir->file_head->ownername, dir->ownername_wmax);
 		display_groupname(dir->file_head->groupname, dir->groupname_wmax);
 		display_size(dir->file_head->stat->st_size, dir->size_wmax);
 		display_mtim(dir->file_head->stat->st_mtimespec.tv_sec);
-		display_filename(dir->file_head->name, dir->file_head->namlen, 1);
+		display_filename(dir->file_head, 1);
 		dir->file_head = dir->file_head->next;
 	}
 }
