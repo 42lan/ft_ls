23e6c64fc0beb866470125fdbba615cf5d3a8525
4slan <amalsago@student.42.fr>
2019-08-19 18:52:56 +0200

    printing linked target if file is a symlink

--- sources/output/display_filename.c
+++ sources/output/display_filename.c
@@ -6,13 +6,26 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:18:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/12 15:45:59 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/19 18:18:46 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_filename(char *filename, int namlen, int width)
+extern t_argp	g_argp[];
+
+void			display_filename(t_file *file, int width)
 {
-	ft_printf("%*s\n", namlen + width, filename);
+	char		*target;
+	int			target_len;
+
+	if (g_argp[LONG_FORMAT].active && S_ISLNK(file->stat->st_mode))
+	{
+		target_len = (file->stat->st_size == 0) ? PATH_MAX : file->stat->st_size;
+		target = ft_strnew(target_len);
+		readlink(file->relpath, target, target_len);
+		ft_printf("%*s -> %s\n", file->namlen + width, file->name, target);
+	}
+	else
+		ft_printf("%*s\n", file->namlen + width, file->name);
 }
