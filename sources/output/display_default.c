e730eaeac2c4acb1719e28acc2d3f45e0388537c
4slan <amalsago@student.42.fr>
2019-08-19 18:36:30 +0200

    passing t_file to display_filename() as prototyped

--- sources/output/display_default.c
+++ sources/output/display_default.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 03:21:49 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/12 15:48:45 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/19 17:22:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void					display_default(t_dir *dir)
 	{
 		//place_cursor(x, y);
 		if (dir->file_head->next == NULL)
-			display_filename(dir->file_head->name, dir->file_head->namlen, 0);
+			display_filename(dir->file_head, 0);
 		else
-			display_filename(dir->file_head->name, dir->file_head->namlen, 0);
+			display_filename(dir->file_head, 0);
 		dir->file_head = dir->file_head->next;
 	}
 }
