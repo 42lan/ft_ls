b2842be041b8931df9e38dd4acb73b94581007a9
4slan <amalsago@student.42.fr>
2019-09-01 20:01:25 +0200

    refactoring: getting list of arguments as a file, sorting them and checking their existensse, so that ft_ls() works properly

--- sources/main.c
+++ sources/main.c
@@ -6,14 +6,43 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:51:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/02 02:44:25 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/01 19:46:35 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int		main(int ac, char **av)
+t_argp			g_argp[] =
 {
-	ft_ls(ac - 1, av + 1);
+	{'l', 0, "List in long format"},
+	{'a', 0, "List files whose names begin with a dot (.)"},
+	{'R', 0, "Recursively list subdirectories encountered"},
+	{'r', 0, "Reverse the order of the sort"},
+	{'1', 0, "List one file per line"},
+	{'t', 0, "Sort by time modified (most recently modified first)"},
+	{0, 0, NULL}
+};
+
+void			print_argument_files(t_file *head)
+{
+	while (head)
+	{
+		if (head->next != NULL)
+			ft_printf("%s --> ", head->name, head->next->name);
+		else
+			ft_printf("%s --> NULL\n", head->name);
+		head = head->next;
+	}
+}
+
+int				main(int ac, char **av)
+{
+	t_file		*file_list;
+
+	get_options(ac, av);
+	file_list = get_argument_files(ac, av);
+	ft_mergesort(&file_list, &name_cmp);
+	check_accessibility(&file_list);
+	ft_ls(file_list);
 	return (0);
 }
