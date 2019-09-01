12529a86eece51846ba7a3f4e777f918955c3c4e
4slan <amalsago@student.42.fr>
2019-09-01 19:55:10 +0200

    using iterator which loop untic ac. So that ac from main be used

--- sources/gets/get_options.c
+++ sources/gets/get_options.c
@@ -6,23 +6,26 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/11 14:58:40 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/11 14:58:52 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/22 18:08:30 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	get_options(char ***av)
+void		get_options(int ac, char **av)
 {
-	while (**av)
+	int		i;
+
+	i = 1;
+	while (i < ac)
 	{
-		if (ft_strequ(**av, "--"))
+		if (ft_strequ(av[i], "--"))
 			break;
-		else if (is_option(**av))
-			parse_argp(**av);
+		else if (is_option(av[i]))
+			parse_argp(av[i]);
 		else
 			break;
-		(*av)++;
+		i++;
 	}
 }
 
