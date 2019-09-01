c388bb856e5adbaf1058f4e2e44ca6639952505c
4slan <amalsago@student.42.fr>
2019-09-01 19:49:09 +0200

    using new function to exit program

--- sources/output/display_usage.c
+++ sources/output/display_usage.c
@@ -6,15 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 04:24:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/05 12:14:09 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/08/22 13:35:14 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_usage(char c)
+void		display_usage(char c)
 {
-	ft_printf("ft_ls: illegal option -- %c\n", c);
-	ft_printf("usage: ft_ls [-lRart] [file ...]\n");
-	exit (0);
+	ft_strerror("ft_ls: illegal option -- ");
+	ft_putchar_fd(c, STDERR_FILENO);
+	ft_error_exit("\nusage: ft_ls [-lRart] [file ...]\n");
 }
