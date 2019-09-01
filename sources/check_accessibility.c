95e8056a093776eb97d97197b6cba47e05caa739
4slan <amalsago@student.42.fr>
2019-09-01 19:41:24 +0200

    definition of function that check if file is exist, if no, removing it from list

---
+++ sources/check_accessibility.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_accessibility.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/24 13:40:18 by amalsago          #+#    #+#             */
+/*   Updated: 2019/09/01 18:05:25 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void		check_accessibility(t_file **head)
+{
+	t_file	*bad_file;
+	t_file	**tracer;
+
+	bad_file = NULL;
+	tracer = head;
+	while ((*tracer))
+	{
+		if (get_stat((*tracer)->name, (*tracer)) == -1)
+		{
+			ft_printf("%s : No such file or directory\n", (*tracer)->name);
+			bad_file = *tracer;
+			remove_file(bad_file);
+			*tracer = (*tracer)->next;
+		}
+		else
+			tracer = &(*tracer)->next;
+	}
+}
