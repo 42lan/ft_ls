92ad5e2085534f524f2391aaeba3015c404ca7af
4slan <amalsago@student.42.fr>
2019-09-01 19:45:59 +0200

    function create and return list of entries

---
+++ sources/gets/get_argument_files.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_argument_files.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/22 14:27:39 by amalsago          #+#    #+#             */
+/*   Updated: 2019/09/01 19:45:43 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+t_file			*get_argument_files(int ac, char **av)
+{
+	int			i;
+	t_file		*file;
+	t_file		*head;
+
+	i = 1;
+	head = NULL;
+	while (is_option(av[i]))
+		i++;
+	if (av[i] == NULL)
+		head = new_file(".", NULL);
+	else
+		while (i < ac)
+		{
+			file = new_file(av[i], NULL);
+			if (head == NULL)
+				head = file;
+			push_front(head, file);
+			i++;
+		}
+	return (head);
+}
