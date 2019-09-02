62237ed45662257c71b7f6b18d39d6b6a35b07b4
4slan <amalsago@student.42.fr>
2019-09-02 18:36:09 +0200

    adding const keyword to protect char *

--- sources/output/display_ownername.c
+++ sources/output/display_ownername.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:10:30 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/07 13:01:30 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/02 18:11:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_ownername(char *ownername, size_t width)
+void	display_ownername(const char *ownername, size_t width)
 {
 	ft_printf(" %-*s", width, ownername);
 }
