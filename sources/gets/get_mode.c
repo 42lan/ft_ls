6e2d02b87640c7063014c1b30f59803ed98dd12a
4slan <amalsago@student.42.fr>
2019-09-02 18:34:49 +0200

    new name of modecat

---
+++ sources/gets/get_mode.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_mode.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/23 13:45:50 by amalsago          #+#    #+#             */
+/*   Updated: 2019/09/02 18:25:37 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+char		*get_mode(mode_t st_mode)
+{
+	char	*str;
+
+	str = ft_memalloc(10);
+	str[0] = get_type(st_mode);
+	ft_strcat(str, get_permissions(st_mode, 0));
+	ft_strcat(str, get_permissions(st_mode, 3));
+	ft_strcat(str, get_permissions(st_mode, 6));
+	return (str);
+}
