b3b038856d98e897fc6bd6fa8133f69ff2a3e51a
4slan <amalsago@student.42.fr>
2019-09-02 18:36:45 +0200

    adding const keyword to protect char *

--- sources/output/display_groupname.c
+++ sources/output/display_groupname.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:13:33 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/07 13:02:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/02 18:11:31 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	display_groupname(char *groupname, size_t width)
+void	display_groupname(const char *groupname, size_t width)
 {
 	ft_printf("  %-*s", width, groupname);
 }
