81e90298e6213d0cd95e609505989b50afda5f2e
4slan <amalsago@student.42.fr>
2019-09-03 14:43:59 +0200

    palcing regfiles in front of directories

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 11:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/02 15:28:41 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/03 14:42:52 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,77 @@
 
 int			ft_ls(t_file *head)
 {
-	t_file	*file;
+	//t_file	*regular = head;
+	//t_file	*directory = head;
 
-	file = head;
-	while (file)
-	{
-		if (S_ISDIR(file->stat->st_mode))
-		{
-			ft_printf("%s:\n", file->name);
-			browse_dir(file->name);
-			if (file->next != NULL)
-				ft_putchar('\n');
-		}
-		else
-			browse_file(file->name, file);
-		file = file->next;
-	}
 	/*
-	if (ac == 0)
-		browse_dir(".");
-	else
+	while (regular)
 	{
-		get_options(&av);
-		if (*av == NULL)
-			browse_dir(".");
-		else
+		if (S_ISREG(regular->stat->st_mode))
+			browse_file(regular->name, regular);
+		regular = regular->next;
+	}
+	ft_printf("\n");
+	while (directory)
+	{
+		if (S_ISDIR(directory->stat->st_mode))
+			browse_dir(directory->name);
+		directory = directory->next;
+	}
+	*/
+	t_file	*curr = head;
+	t_file	*next = NULL;
+	t_file	*reglst = NULL;
+	t_file	*dirlst = NULL;
+	t_file	*lastdir = NULL;
+	t_file	*lastreg = NULL;
+	while (curr)
+	{
+		next = curr->next;
+		if (S_ISDIR(curr->stat->st_mode))
 		{
-			arg_files = get_files_from_arguments(av);
-			print_arg_files(arg_files);
-			sort_arg_files(arg_files);
-			while (++i < ac)
+			if (dirlst == NULL)
 			{
-				file = new_file();
-				if (get_stat(av[i], file) == 0)
-				{
-					ft_printf("Error get_stat(%s)\n", av[i]);
-					exit(0);
-				}
-				if (S_ISDIR(file->stat->st_mode))
-				{
-					free(file);
-					file = NULL;
-					browse_dir(av[i]);
-				}
-				else
-					browse_file(av[i], file);
+				dirlst = curr;
+				curr = next;
+				lastdir = dirlst;
+				lastdir->next = NULL;
+			}
+			else
+			{
+				lastdir->next = curr;
+				lastdir = curr;
+				lastdir->next = NULL;
+				curr = next;
+			}
+		}
+		else if (S_ISREG(curr->stat->st_mode))
+		{
+			if (reglst == NULL)
+			{
+				reglst = curr;
+				curr = next;
+				lastreg = reglst;
+				lastreg->next = NULL;
+			}
+			else
+			{
+				lastreg->next = curr;
+				lastreg = curr;
+				lastreg->next = NULL;
+				curr = next;
 			}
 		}
 	}
-	*/
+	lastreg->next = dirlst;
+	head = reglst;
+	while (head)
+	{
+		if (head->next == NULL)
+			ft_printf("%s -> NULL\n", head->name);
+		else
+			ft_printf("%s -> ", head->name);
+		head = head->next;
+	}
 	return (1);
 }
