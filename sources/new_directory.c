01c5fba3bba6eb1e1efac11b41d32e415239cf5a
4slan <amalsago@student.42.fr>
2019-09-03 10:21:02 +0200

    subdir_head is new name fo sdir_head

--- sources/new_directory.c
+++ sources/new_directory.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 15:39:53 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/02 16:55:17 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/03 09:51:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,6 @@ t_dir		*new_directory(const char *name)
 	directory->ownername_wmax = 0;
 	directory->groupname_wmax = 0;
 	directory->file_head = NULL;
-	directory->sdir_head = NULL;
+	directory->subdir_head = NULL;
 	return (directory);
 }
