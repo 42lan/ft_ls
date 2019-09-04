98268d023ee8ba787b8e1eb910be98ae47cf2770
4slan <amalsago@student.42.fr>
2019-09-04 12:21:19 +0200

    adding condition to put padding before displaying filename

--- sources/output/display_filename.c
+++ sources/output/display_filename.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:18:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/04 10:20:31 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/04 11:49:18 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,23 @@
 
 extern t_argp	g_argp[];
 
-void			display_filename(t_file *file, int width)
+void			display_filename(t_file *file)
 {
 	char		*target;
 	int			target_len;
 
-	if (g_argp[LONG_FORMAT].active && S_ISLNK(file->stat->st_mode))
+	if (g_argp[LONG_FORMAT].active)
 	{
-		target_len = (file->stat->st_size == 0) ? PATH_MAX : file->stat->st_size;
-		target = ft_strnew(target_len);
-		readlink(file->relpath, target, target_len);
-		ft_printf("%*s -> %s\n", file->namlen + width, file->name, target);
+		if (S_ISLNK(file->stat->st_mode))
+		{
+			target_len = (file->stat->st_size == 0) ? PATH_MAX : file->stat->st_size;
+			target = ft_strnew(target_len);
+			readlink(file->relpath, target, target_len);
+			ft_printf(" %s -> %s\n", file->name, target);
+		}
+		else
+			printf(" %s\n", file->name);
 	}
 	else
-		ft_printf("%*s\n", width, file->name);
+		printf("%s\n", file->name);
 }
