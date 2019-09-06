3e405c9244f3fe09c77c29ae8bc503a622dafdf3
4slan <amalsago@student.42.fr>
2019-09-06 14:00:15 +0200

    removing extra space between mode and number of links

--- sources/output/display_nlink.c
+++ sources/output/display_nlink.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:05:19 by amalsago          #+#    #+#             */
-/*   Updated: 2019/07/14 06:01:03 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/06 13:42:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	display_nlink(nlink_t st_nlink, int width)
 {
-	ft_printf("%*d", width + 2, st_nlink);
+	ft_printf("%*d", width + 1, st_nlink);
 }
