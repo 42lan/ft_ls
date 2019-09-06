37880c01a606f70ba4ea4fc50e438cb8278b9522
4slan <amalsago@student.42.fr>
2019-09-06 13:59:51 +0200

    adding set for extended attribute - link

--- sources/gets/get_mode.c
+++ sources/gets/get_mode.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 13:45:50 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/05 18:45:34 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/06 13:54:42 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ char		*get_mode(mode_t st_mode)
 {
 	char	*str;
 
-	str = ft_memalloc(10);
+	str = ft_memalloc(12);
 	str[0] = get_type(st_mode);
 	ft_strcat(str, get_permissions(st_mode, 0));
 	ft_strcat(str, get_permissions(st_mode, 3));
 	ft_strcat(str, get_permissions(st_mode, 6));
 	set_special_permissions(st_mode, &str);
+	//set_extended_attributes();
+	str[10] = (S_ISLNK(st_mode)) ? '@' : ' ';
 	return (str);
 }
