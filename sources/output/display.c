dae5e2976f968dec49e4dcdfa34274c15647d239
4slan <amalsago@student.42.fr>
2019-09-07 15:59:50 +0200

    calling functions with new name

--- sources/output/display.c
+++ sources/output/display.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 21:07:44 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/08 13:24:57 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/07 14:18:08 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ extern t_argp	g_argp[];
 void			display(t_dir *current_dir)
 {
 	if (g_argp[LONG_FORMAT].active == 1)
-		display_long(current_dir);
+		print_long(current_dir);
 	else
-		display_default(current_dir);
+		print_default(current_dir);
 }
