9e3b27da732dc254de33da572b5518cd5189026d
4slan <amalsago@student.42.fr>
2019-09-07 20:13:53 +0200

    handeling symbolic links

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,12 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 11:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/03 15:12:49 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/07 19:29:40 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+extern t_argp		g_argp[];
 
 int			ft_ls(t_file *head)
 {
@@ -20,10 +21,20 @@ int			ft_ls(t_file *head)
 	file = head;
 	while (file)
 	{
+		if (g_argp[LONG_FORMAT].active == 1 && file->name[file->namlen - 1] == '/')
+			stat(file->name, file->stat);
 		if (S_ISDIR(file->stat->st_mode))
 			browse_dir(file->name);
-		else if (S_ISREG(file->stat->st_mode))
+		else
+		{
+			if (S_ISLNK(file->stat->st_mode))
+			{
+				readlink(file->name, file->target, sizeof(file->target) - 1);
+				file->targetlen = ft_strlen(file->target);
+				file->target[file->targetlen] = '\0';
+			}
 			browse_file(file->name, file);
+		}
 		file = file->next;
 	}
 	return (1);
