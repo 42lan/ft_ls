33a21fd6602a5e7d2bd4b3e8f7d5178127a19247
4slan <amalsago@student.42.fr>
2019-09-07 20:15:03 +0200

    removing readlink() fromm here because it is called in browse_dir()

--- sources/output/print_filename.c
+++ sources/output/print_filename.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:18:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/07 13:31:00 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/07 19:06:10 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,10 @@ extern t_argp	g_argp[];
 
 void			print_filename(t_file *file)
 {
-	char		*target;
-	int			target_len;
-
 	if (g_argp[LONG_FORMAT].active)
 	{
 		if (S_ISLNK(file->stat->st_mode))
-		{
-			if (file->stat->st_size == 0)
-				target_len = PATH_MAX;
-			else
-				target_len = file->stat->st_size;
-			target = ft_strnew(target_len);
-			readlink(file->relpath, target, target_len);
-			ft_printf(" %s -> %s\n", file->name, target);
-		}
+			ft_printf(" %s -> %s\n", file->name, file->target);
 		else
 			ft_printf(" %s\n", file->name);
 	}
