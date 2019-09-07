8f8c45e59a8afba7f2e1e77341b8512534317a92
4slan <amalsago@student.42.fr>
2019-09-07 20:14:28 +0200

    renaming current_dir into directory just for fun

--- sources/output/display.c
+++ sources/output/display.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 21:07:44 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/07 14:18:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/07 19:59:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 extern t_argp	g_argp[];
 
-void			display(t_dir *current_dir)
+void			display(t_dir *directory)
 {
 	if (g_argp[LONG_FORMAT].active == 1)
-		print_long(current_dir);
+		print_long(directory);
 	else
-		print_default(current_dir);
+		print_default(directory);
 }
