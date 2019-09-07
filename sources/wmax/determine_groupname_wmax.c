e0219b79537ada3b40144b2f9658984df911b909
4slan <amalsago@student.42.fr>
2019-09-07 20:16:19 +0200

    renaming current_dir into directory just for fun

--- sources/wmax/determine_groupname_wmax.c
+++ sources/wmax/determine_groupname_wmax.c
@@ -6,17 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 12:53:31 by amalsago          #+#    #+#             */
-/*   Updated: 2019/08/07 12:57:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/07 20:00:04 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void		determine_groupname_wmax(t_file *file, t_dir *current_dir)
+void		determine_groupname_wmax(t_file *file, t_dir *directory)
 {
 	size_t	groupname_w;
 
 	groupname_w = ft_strlen(file->groupname);
-	if (groupname_w > current_dir->groupname_wmax)
-		current_dir->groupname_wmax = groupname_w;
+	if (groupname_w > directory->groupname_wmax)
+		directory->groupname_wmax = groupname_w;
 }
