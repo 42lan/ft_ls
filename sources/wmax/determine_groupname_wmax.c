de38cb4d007fb2fc6b9a238214a19656fd38c3bc
4slan <amalsago@student.42.fr>
2019-09-08 15:54:22 +0200

    sources/wmax/determine_namlen_wmax.c

--- sources/wmax/determine_groupname_wmax.c
+++ sources/wmax/determine_groupname_wmax.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 12:53:31 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/07 20:00:04 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/08 10:05:17 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void		determine_groupname_wmax(t_file *file, t_dir *directory)
+void		determine_groupname_wmax(t_dir *directory, t_file *file)
 {
 	size_t	groupname_w;
 
