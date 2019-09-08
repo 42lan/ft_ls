eb57aec73cc1e21dabdf7f9eeaaf7dafce3676b8
4slan <amalsago@student.42.fr>
2019-09-08 15:58:09 +0200

    refactoring definition and adding new function that set extd attributes

--- sources/gets/get_mode.c
+++ sources/gets/get_mode.c
@@ -6,23 +6,29 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 13:45:50 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/06 13:54:42 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/08 13:48:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-char		*get_mode(mode_t st_mode)
+void	set_extended_attribute(t_file *file)
 {
-	char	*str;
+	if (listxattr(file->relpath, NULL, 0, XATTR_NOFOLLOW) > 0)
+		file->mode[10] = '@';
+	else if (acl_get_file(file->relpath, ACL_TYPE_EXTENDED) != NULL)
+		file->mode[10] = '+';
+	else
+		file->mode[10] = ' ';
+}
 
-	str = ft_memalloc(12);
-	str[0] = get_type(st_mode);
-	ft_strcat(str, get_permissions(st_mode, 0));
-	ft_strcat(str, get_permissions(st_mode, 3));
-	ft_strcat(str, get_permissions(st_mode, 6));
-	set_special_permissions(st_mode, &str);
-	//set_extended_attributes();
-	str[10] = (S_ISLNK(st_mode)) ? '@' : ' ';
-	return (str);
+void	get_mode(t_file *file)
+{
+	file->mode = ft_memalloc(12);
+	file->mode[0] = get_type(file->stat->st_mode);
+	ft_strcat(file->mode, get_permissions(file->stat->st_mode, 0));
+	ft_strcat(file->mode, get_permissions(file->stat->st_mode, 3));
+	ft_strcat(file->mode, get_permissions(file->stat->st_mode, 6));
+	set_special_permissions(file->stat->st_mode, &file->mode);
+	set_extended_attribute(file);
 }
