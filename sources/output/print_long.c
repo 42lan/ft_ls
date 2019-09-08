8a2d9fdf533868befb6ff63bff37ffdd92dcadbf
4slan <amalsago@student.42.fr>
2019-09-08 15:57:00 +0200

    passing stored mode to print

--- sources/output/print_long.c
+++ sources/output/print_long.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/07 15:56:52 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/08 13:40:39 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ void	print_long(t_dir *dir)
 			print_totalblocks(dir->total_blocks);
 		while (dir->file_head != NULL)
 		{
-			print_mode(dir->file_head->stat->st_mode);
-			//print_acl();
+			print_mode(dir->file_head->mode);
 			print_nlink(dir->file_head->stat->st_nlink, dir->nlink_wmax);
 			print_ownername(dir->file_head->ownername, dir->ownername_wmax);
 			print_groupname(dir->file_head->groupname, dir->groupname_wmax);
