487f176126827a7644e4461f7111852a03d3add9
4slan <amalsago@student.42.fr>
2019-09-08 15:57:43 +0200

    removing == 1 because in some function that takes more than 80 char on line

--- sources/output/display.c
+++ sources/output/display.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 21:07:44 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/07 19:59:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/08 10:54:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ extern t_argp	g_argp[];
 
 void			display(t_dir *directory)
 {
-	if (g_argp[LONG_FORMAT].active == 1)
+	if (g_argp[LONG_FORMAT].active)
 		print_long(directory);
 	else
 		print_default(directory);
