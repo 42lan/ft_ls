681548d61fbe6ac38c4fc0be99a8182a25dcc19d
4slan <amalsago@student.42.fr>
2019-09-08 15:51:11 +0200

    holding mode in file struct; changing some prototypes

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:53:19 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/07 19:53:21 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/08 13:44:18 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <sys/acl.h>
 # include <sys/ioctl.h>
 # include <sys/stat.h>
 # include <sys/types.h>
@@ -45,7 +46,6 @@ typedef struct		s_dir
 	char			*groupname;
 	struct s_file	*file_head;
 	struct s_file	*subdir_head;
-	size_t			nb_files;
 	size_t			total_blocks;
 	size_t			namlen_wmax;
 	size_t			nlink_wmax;
@@ -62,6 +62,7 @@ typedef struct		s_file
 	char			*relpath;
 	char			target[PATH_MAX];
 	size_t			targetlen;
+	char			*mode;
 	struct stat		*stat;
 	char			*ownername;
 	char			*groupname;
@@ -81,7 +82,7 @@ void				browse_dir(const char *path);
 void				browse_file(const char *path, t_file *file);
 int					ft_ls(t_file *head);
 
-void				fill_struct(t_file *file, struct dirent *dirent);
+void				fill_struct(t_file *file);
 t_file				*new_file(const char *name, const char *path);
 t_dir				*new_directory(const char *path);
 
@@ -90,7 +91,7 @@ void				print_default(t_dir *current_dir);
 void				print_long(t_dir *current_dir);
 void				print_filename(t_file *file);
 void				print_groupname(const char *groupname, size_t width);
-void				print_mode(mode_t st_mode);
+void				print_mode(const char *mode);
 void				print_mtime(time_t tv_sec);
 void				print_nlink(nlink_t st_nlink, int width);
 void				print_options(t_argp g_argp[]);
@@ -114,19 +115,18 @@ void				remove_file(t_file *file);
 void				reverse_files(t_file **head);
 void				set_special_permissions(mode_t st_mode, char **str);
 
-void				determine_groupname_wmax(t_file *file, t_dir *current_dir);
-void				determine_namlen_wmax(struct dirent *dirent,
-					t_dir *current_dir);
-void				determine_nlink_wmax(t_file *file, t_dir *current_dir);
-void				determine_ownername_wmax(t_file *file, t_dir *current_dir);
-void				determine_size_wmax(t_file *file, t_dir *current_dir);
-void				determine_wmax(struct dirent *dirent, t_file *file,
-					t_dir *current_dir);
+void				determine_groupname_wmax(t_dir *directory, t_file *file);
+void				determine_namlen_wmax(t_dir *directory, t_file *file);
+void				determine_nlink_wmax(t_dir *directory, t_file *file);
+void				determine_ownername_wmax(t_dir *directory, t_file *file);
+void				determine_size_wmax(t_dir *directory, t_file *file);
+void				determine_wmax(t_dir *directory, t_file *file);
 
 t_file				*get_argument_files(int ac, char **av);
 void				get_options(int ac, char **av);
 char				get_type(mode_t mode);
-char				*get_mode(mode_t st_mode);
+void				get_link(t_file *file);
+void				get_mode(t_file *file);
 int					get_stat(const char *path, t_file *file);
 struct passwd		*get_pwstruct(uid_t st_uid);
 struct group		*get_grstruct(gid_t st_gid);
