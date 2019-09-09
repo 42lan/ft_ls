50eecc71db9a7caea4632d057e1567f1fa4b9244
4slan <amalsago@student.42.fr>
2019-09-09 16:30:20 +0200

    removing extra space

--- sources/gets/get_major_minor.c
+++ sources/gets/get_major_minor.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 13:19:19 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/09 13:19:28 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/09 13:38:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void				get_major_minor(t_file *file)
+void	get_major_minor(t_file *file)
 {
 	file->major = major(file->stat->st_rdev);
 	file->minor = minor(file->stat->st_rdev);
