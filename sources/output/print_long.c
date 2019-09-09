4a27ea45a6a7da873480c0b291943d3368fa49d2
4slan <amalsago@student.42.fr>
2019-09-09 13:09:49 +0200

    handeling major-minor printing of /dev, otherwise print size of file

--- sources/output/print_long.c
+++ sources/output/print_long.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/08 13:40:39 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/09 12:41:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,10 @@ void	print_long(t_dir *dir)
 			print_nlink(dir->file_head->stat->st_nlink, dir->nlink_wmax);
 			print_ownername(dir->file_head->ownername, dir->ownername_wmax);
 			print_groupname(dir->file_head->groupname, dir->groupname_wmax);
-			print_size(dir->file_head->stat->st_size, dir->size_wmax);
+			if (ft_strstr(dir->name, "/dev"))
+				print_major_minor(dir, dir->file_head->major, dir->file_head->minor);
+			else
+				print_size(dir->file_head->stat->st_size, dir->size_wmax);
 			print_mtime(dir->file_head->stat->st_mtimespec.tv_sec);
 			print_filename(dir->file_head);
 			dir->file_head = dir->file_head->next;
