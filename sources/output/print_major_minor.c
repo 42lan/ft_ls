e707912ab7b70879ccdd6a5810cef32840f8c28a
4slan <amalsago@student.42.fr>
2019-09-09 16:39:47 +0200

    using size_t instad ssize_t

--- sources/output/print_major_minor.c
+++ sources/output/print_major_minor.c
@@ -6,13 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 12:01:43 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/09 12:42:36 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/09 16:39:00 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	print_major_minor(t_dir *dir, size_t major, ssize_t minor)
+void	print_major_minor(t_dir *dir, size_t major, size_t minor)
 {
 	ft_printf("%*zu, %*zu", dir->major_wmax + 3, major, dir->minor_wmax, minor);
 }
