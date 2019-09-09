2b5778e73e36bb8296c9c52607b7fd3b3c1765f1
4slan <amalsago@student.42.fr>
2019-09-09 16:40:54 +0200

    using size_t instad ssize_t

--- sources/wmax/determine_major_minor.c
+++ sources/wmax/determine_major_minor.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 12:54:53 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/09 12:55:05 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/09 16:40:23 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void			determine_major_minor_wmax(t_dir *directory, t_file *file)
 {
-	ssize_t		major_w;
-	ssize_t		minor_w;
+	size_t		major_w;
+	size_t		minor_w;
 
 	major_w = ft_uilen(file->major, 10);
 	minor_w = ft_uilen(file->minor, 10);
