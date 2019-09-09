e9a560174f4d5210b73ab3f702186e762403c073
4slan <amalsago@student.42.fr>
2019-09-09 12:59:25 +0200

    placing set_extended_attribute() in separate file

--- sources/gets/get_mode.c
+++ sources/gets/get_mode.c
@@ -6,21 +6,11 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 13:45:50 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/08 13:48:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/09 10:50:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-
-void	set_extended_attribute(t_file *file)
-{
-	if (listxattr(file->relpath, NULL, 0, XATTR_NOFOLLOW) > 0)
-		file->mode[10] = '@';
-	else if (acl_get_file(file->relpath, ACL_TYPE_EXTENDED) != NULL)
-		file->mode[10] = '+';
-	else
-		file->mode[10] = ' ';
-}
 
 void	get_mode(t_file *file)
 {
