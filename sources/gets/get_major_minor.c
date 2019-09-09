d5fcfdc31d1e249da5163915bf6b546fe1b3b4dc
4slan <amalsago@student.42.fr>
2019-09-09 13:21:06 +0200

    placing definiton of function in separete file

---
+++ sources/gets/get_major_minor.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_major_minor.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/09 13:19:19 by amalsago          #+#    #+#             */
+/*   Updated: 2019/09/09 13:19:28 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void				get_major_minor(t_file *file)
+{
+	file->major = major(file->stat->st_rdev);
+	file->minor = minor(file->stat->st_rdev);
+}
