ec55ea266a302dc4bd144b78361d3f6fe8c2d793
4slan <amalsago@student.42.fr>
2019-09-09 13:24:16 +0200

    calling new function to determine max with of major and minor numbers

--- sources/wmax/determine_wmax.c
+++ sources/wmax/determine_wmax.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 15:05:30 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/08 09:49:39 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/09 12:54:41 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 ** from stat struct.
 */
 
+
 void	determine_wmax(t_dir *directory, t_file *file)
 {
 	determine_namlen_wmax(directory, file);
@@ -25,4 +26,5 @@ void	determine_wmax(t_dir *directory, t_file *file)
 	determine_groupname_wmax(directory, file);
 	determine_nlink_wmax(directory, file);
 	determine_size_wmax(directory, file);
+	determine_major_minor_wmax(directory, file);
 }
