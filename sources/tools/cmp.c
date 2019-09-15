6e66e271253e2635f1367e1d6d80be2f2f301d94
4slan <amalsago@student.42.fr>
2019-09-15 12:53:09 +0200

    handeling case when files are modified at the same second - so using nsec to compare

--- sources/tools/cmp.c
+++ sources/tools/cmp.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/01 11:38:16 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/01 11:38:20 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/15 12:07:31 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 int		mtime_cmp(t_file *file_a, t_file *file_b)
 {
-	return (file_a->stat->st_mtime - file_b->stat->st_mtime);
+	if (file_a->stat->st_mtime == file_b->stat->st_mtime)
+		return (file_a->stat->st_mtimespec.tv_nsec > file_b->stat->st_mtimespec.tv_nsec);
+	else
+		return (file_a->stat->st_mtime - file_b->stat->st_mtime);
 }
 
 int		name_cmp(t_file *file_a, t_file *file_b)
