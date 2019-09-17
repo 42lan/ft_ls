ead8f1caf6c0973d3d8008e3bd27bfb32c4018b5
4slan <amalsago@student.42.fr>
2019-09-17 13:13:10 +0200

    refactoring definition to be more flexible with removing ft_strchr()

--- sources/tools/parse_argp.c
+++ sources/tools/parse_argp.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 18:31:16 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/07 15:55:45 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/17 13:09:40 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ void			parse_argp(const char *av)
 {
 	int			i;
 	int			k;
+	int			found;
 
 	i = 0;
-	k = -1;
 	while (av[++i] != '\0')
 	{
 		k = -1;
-		if (ft_strchr("lRart1", av[i]) == NULL)
-			print_usage(av[i]);
+		found = 0;
 		while (g_argp[++k].sign != 0)
 			if (g_argp[k].sign == av[i])
+			{
 				g_argp[k].active = 1;
+				found = 1;
+			}
+		if (found == 0)
+			print_usage(av[i]);
 	}
 }
