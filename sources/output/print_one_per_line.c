01dcd4144d71f47657619eac4d15deabcd546157
4slan <amalsago@student.42.fr>
2019-09-21 17:56:28 +0200

    definition of function that prints one file per line

---
+++ sources/output/print_one_per_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_one_per_line.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/21 17:46:56 by amalsago          #+#    #+#             */
+/*   Updated: 2019/09/21 17:52:51 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void	print_one_per_line(t_dir *directory)
+{
+	if (directory->file_head)
+		while (directory->file_head != NULL)
+		{
+			print_filename(directory->file_head, 0);
+			ft_printf("\n");
+			directory->file_head = directory->file_head->next;
+		}
+}
