e89943f3ac67ea388619a18e3b418852440db513
4slan <amalsago@student.42.fr>
2019-09-21 17:59:08 +0200

    renaming dir into directory

--- sources/output/print_long.c
+++ sources/output/print_long.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/21 15:12:51 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/21 17:50:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 extern t_argp	g_argp[];
 
-void	print_long(t_dir *dir)
+void	print_long(t_dir *directory)
 {
-	if (dir->file_head)
+	if (directory->file_head)
 	{
-		if (dir->file_head->next != NULL)
-			print_totalblocks(dir->total_blocks);
-		while (dir->file_head != NULL)
+		if (directory->file_head->next != NULL)
+			print_totalblocks(directory->total_blocks);
+		while (directory->file_head != NULL)
 		{
-			print_mode(dir->file_head->mode);
-			print_nlink(dir->file_head->stat->st_nlink, dir->nlink_wmax);
-			print_ownername(dir->file_head->ownername, dir->ownername_wmax);
-			print_groupname(dir->file_head->groupname, dir->groupname_wmax);
-			if (ft_strstr(dir->name, "/dev") && !(S_ISDIR(dir->file_head->stat->st_mode)))
-				print_major_minor(dir, dir->file_head->major, dir->file_head->minor);
+			print_mode(directory->file_head->mode);
+			print_nlink(directory->file_head->stat->st_nlink, directory->nlink_wmax);
+			print_ownername(directory->file_head->ownername, directory->ownername_wmax);
+			print_groupname(directory->file_head->groupname, directory->groupname_wmax);
+			if (ft_strstr(directory->name, "/dev") && !(S_ISDIR(directory->file_head->stat->st_mode)))
+				print_major_minor(directory, directory->file_head->major, directory->file_head->minor);
 			else
-				print_size(dir->file_head->stat->st_size, dir->size_wmax);
-			print_mtime(dir->file_head->stat->st_mtimespec.tv_sec);
-			print_filename(dir->file_head, 0);
-			dir->file_head = dir->file_head->next;
+				print_size(directory->file_head->stat->st_size, directory->size_wmax);
+			print_mtime(directory->file_head->stat->st_mtimespec.tv_sec);
+			print_filename(directory->file_head, 0);
+			directory->file_head = directory->file_head->next;
 			ft_printf("\n");
 		}
 	}
