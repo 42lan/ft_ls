720b7e77dcc32375b1ec56f1109c6dff2eae9255
4slan <amalsago@student.42.fr>
2019-09-21 16:11:06 +0200

    printing major/minor only for non directories; adding new line between files

--- sources/output/print_long.c
+++ sources/output/print_long.c
@@ -6,11 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/09 12:41:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/21 15:12:51 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+extern t_argp	g_argp[];
 
 void	print_long(t_dir *dir)
 {
@@ -24,13 +26,14 @@ void	print_long(t_dir *dir)
 			print_nlink(dir->file_head->stat->st_nlink, dir->nlink_wmax);
 			print_ownername(dir->file_head->ownername, dir->ownername_wmax);
 			print_groupname(dir->file_head->groupname, dir->groupname_wmax);
-			if (ft_strstr(dir->name, "/dev"))
+			if (ft_strstr(dir->name, "/dev") && !(S_ISDIR(dir->file_head->stat->st_mode)))
 				print_major_minor(dir, dir->file_head->major, dir->file_head->minor);
 			else
 				print_size(dir->file_head->stat->st_size, dir->size_wmax);
 			print_mtime(dir->file_head->stat->st_mtimespec.tv_sec);
-			print_filename(dir->file_head);
+			print_filename(dir->file_head, 0);
 			dir->file_head = dir->file_head->next;
+			ft_printf("\n");
 		}
 	}
 }
