09001a038749ef80362a0f08187c74f0dd9140c9
4slan <amalsago@student.42.fr>
2019-09-22 11:17:38 +0200

    get_stat() is placed in fill_struct()

--- sources/browse_directory.c
+++ sources/browse_directory.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 21:42:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/21 12:56:50 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/22 11:13:04 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,6 @@ static void			loop_through(DIR *dp, t_dir *directory, const char *path)
 			continue ;
 		directory->nb_files++;
 		file = new_file(path, dirent->d_name);
-		get_stat(file->relpath, file);
 		fill_struct(file);
 		determine_wmax(directory, file);
 		if (!(ft_strequ(file->name, ".") || ft_strequ(file->name, "..")))
