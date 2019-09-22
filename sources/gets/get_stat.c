bc818ba64ef0c4a76bd143e11ed65d049a440ee5
4slan <amalsago@student.42.fr>
2019-09-22 16:05:05 +0200

    assigning value to path according to if there is a file from argv or from subdir

--- sources/gets/get_stat.c
+++ sources/gets/get_stat.c
@@ -6,16 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/24 12:16:53 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/01 12:36:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/22 11:24:25 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int		get_stat(const char *path, t_file *file)
+int			get_stat(t_file *file)
 {
-	if (path == NULL)
-		return (0);
+	char	*path;
+
+	path = (file->relpath == NULL) ? file->name : file->relpath;
 	if (!(file->stat = (struct stat *)ft_memalloc(sizeof(struct stat))))
 		return (0);
 	return (lstat(path, file->stat));
