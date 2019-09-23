01c8887aa89bbac2faa9795dfd087eaa80784cec
4slan <amalsago@student.42.fr>
2019-09-23 15:17:49 +0200

    restoring previous concatenanig instructions

--- sources/tools/append_indicator.c
+++ sources/tools/append_indicator.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 11:16:11 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/23 13:18:48 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/23 15:05:19 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	append_indicator(t_file *file)
 {
 	if (S_ISDIR(file->stat->st_mode))
-		ft_putchar('/');
+		ft_strcat(file->name, "/");
 	else if (S_ISREG(file->stat->st_mode) && file->mode[3] == 'x')
-		ft_putchar('*');
+		ft_strcat(file->name, "*");
 	else if (S_ISLNK(file->stat->st_mode))
-		ft_putchar('@');
+		ft_strcat(file->name, "@");
 	else if (S_ISSOCK(file->stat->st_mode))
-		ft_putchar('=');
+		ft_strcat(file->name, "=");
 	else if (S_ISFIFO(file->stat->st_mode))
-		ft_putchar('|');
+		ft_strcat(file->name, "|");
 }
