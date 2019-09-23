ceb8dae9d6f39d2ee489c23e6d343510b528b73f
4slan <amalsago@student.42.fr>
2019-09-23 13:25:30 +0200

    printing corresponding symbol instead concatenating it which take more time

--- sources/tools/append_indicator.c
+++ sources/tools/append_indicator.c
@@ -6,25 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 11:16:11 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/17 13:44:14 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/23 13:18:48 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void			append_indicator(t_file *file)
+void	append_indicator(t_file *file)
 {
 	if (S_ISDIR(file->stat->st_mode))
-		ft_strcat(file->name, "/");
-	else if (S_ISREG(file->stat->st_mode) && file->mode[3] == 'x')
-		ft_strcat(file->name, "*");
-	else if (S_ISLNK(file->stat->st_mode))
-		ft_strcat(file->name, "@");
-	else if (S_ISSOCK(file->stat->st_mode))
-		ft_strcat(file->name, "=");
-	else if (S_ISFIFO(file->stat->st_mode))
-		ft_strcat(file->name, "|");
-	/*if (S_ISDIR(file->stat->st_mode))
 		ft_putchar('/');
 	else if (S_ISREG(file->stat->st_mode) && file->mode[3] == 'x')
 		ft_putchar('*');
@@ -34,9 +24,4 @@ void			append_indicator(t_file *file)
 		ft_putchar('=');
 	else if (S_ISFIFO(file->stat->st_mode))
 		ft_putchar('|');
-	*/
-	/*
-	else if (S_ISSSSSSSSSSSSSSSSSSSSSSSSSS(file->stat->st_mode))
-		ft_putchar('%');
-	*/
 }
