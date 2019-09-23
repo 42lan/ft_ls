7a91e9505487d8ae5d227e5ce3e5da08a28e50b6
4slan <amalsago@student.42.fr>
2019-09-23 13:36:32 +0200

    adding new local variable to simplify access to t_file field

--- sources/output/print_long.c
+++ sources/output/print_long.c
@@ -6,39 +6,42 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/23 13:18:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/23 13:35:32 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-extern t_argp		g_argp[];
+extern t_argp	g_argp[];
 
-void	print_long(t_dir *directory)
+void			print_long(t_dir *directory)
 {
-	if (directory->file_head)
+	t_file		*file;
+
+	file = directory->file_head;
+	if (file)
 	{
-		if (directory->file_head->next != NULL)
+		if (file->next != NULL)
 			print_totalblocks(directory->total_blocks);
-		while (directory->file_head != NULL)
+		while (file != NULL)
 		{
-			print_mode(directory->file_head->mode);
-			print_nlink(directory->file_head->stat->st_nlink, directory->nlink_wmax);
-			print_ownername(directory->file_head->ownername, directory->ownername_wmax);
-			print_groupname(directory->file_head->groupname, directory->groupname_wmax);
-			if (ft_strequ(directory->name, "/dev") && !(S_ISDIR(directory->file_head->stat->st_mode)))
-				print_major_minor(directory, directory->file_head->major, directory->file_head->minor);
+			print_mode(file->mode);
+			print_nlink(file->stat->st_nlink, directory->nlink_wmax);
+			print_ownername(file->ownername, directory->ownername_wmax);
+			print_groupname(file->groupname, directory->groupname_wmax);
+			if (ft_strequ(directory->name, "/dev") && !(S_ISDIR(file->stat->st_mode)))
+				print_major_minor(directory, file->major, file->minor);
 			else
-				print_size(directory->file_head->stat->st_size,
+				print_size(file->stat->st_size,
 						(ft_strequ(directory->name, "/dev"))
-						 && (S_ISDIR(directory->file_head->stat->st_mode)
-						|| S_ISLNK(directory->file_head->stat->st_mode))
+						 && (S_ISDIR(file->stat->st_mode)
+						|| S_ISLNK(file->stat->st_mode))
 						 ? directory->size_wmax + 7 : directory->size_wmax);
-			print_mtime(directory->file_head->stat->st_mtimespec.tv_sec);
-			print_filename(directory->file_head, 0);
+			print_mtime(file->stat->st_mtimespec.tv_sec);
+			print_filename(file, 0);
 			if (g_argp[INDICATOR].active)
-				append_indicator(directory->file_head);
-			directory->file_head = directory->file_head->next;
+				append_indicator(file);
+			file = file->next;
 			ft_printf("\n");
 		}
 	}
