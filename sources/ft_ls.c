0ee4271d18e68a46d6d3286507bdb1129bd388a6
4slan <amalsago@student.42.fr>
2019-09-23 11:11:12 +0200

    handeling display name of directory if there is no only one dir

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 11:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/22 15:59:32 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/23 11:08:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,37 @@
 
 extern t_argp		g_argp[];
 
+static int			check_for_one_dir(t_file *head)
+{
+	t_file			*tmp;
+	int				one_dir;
+
+	tmp = head;
+	one_dir = 1;
+	while (tmp && one_dir == 1)
+	{
+		if (S_ISDIR(tmp->stat->st_mode))
+			if (tmp->next && S_ISDIR(tmp->next->stat->st_mode))
+				one_dir = 0;
+		tmp = tmp->next;
+	}
+	return (one_dir);
+}
+
 int					ft_ls(t_file *head)
 {
+	int				one_dir;
 	t_file			*file;
 
 	file = head;
+	one_dir = check_for_one_dir(file);
 	while (file)
 	{
 		if (g_argp[LONG_FORMAT].active && file->name[file->namlen - 1] == '/')
 			stat(file->name, file->stat);
 		if (S_ISDIR(file->stat->st_mode))
 		{
-			ft_printf("%s:\n", file->name);
+			(one_dir != 1) ? ft_printf("%s:\n", file->name) : 0;
 			browse_directory(file->name);
 		}
 		else
