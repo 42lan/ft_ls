3b094c12d535939b77141a58fe826219449c3010
4slan <amalsago@student.42.fr>
2019-09-23 15:17:58 +0200

    removing call to append indicator from here and call it in main function

--- sources/output/print_one_per_line.c
+++ sources/output/print_one_per_line.c
@@ -6,13 +6,11 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 17:46:56 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/23 13:20:51 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/23 14:38:00 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-
-extern t_argp	g_argp[];
 
 void			print_one_per_line(t_dir *directory)
 {
@@ -20,8 +18,6 @@ void			print_one_per_line(t_dir *directory)
 		while (directory->file_head != NULL)
 		{
 			print_filename(directory->file_head, 0);
-			if (g_argp[INDICATOR].active)
-				append_indicator(directory->file_head);
 			ft_putchar('\n');
 			directory->file_head = directory->file_head->next;
 		}
