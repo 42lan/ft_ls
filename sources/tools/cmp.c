90913c84f1e71dac74e0b7b2024f5e9d55565a76
4slan <amalsago@student.42.fr>
2019-09-23 13:12:42 +0200

    refactoring code to be in norm

--- sources/tools/cmp.c
+++ sources/tools/cmp.c
@@ -6,18 +6,21 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/01 11:38:16 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/15 13:11:13 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/23 13:12:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int		mtime_cmp(t_file *a, t_file *b)
+int			mtime_cmp(t_file *a, t_file *b)
 {
+	int		diff;
+
+	diff = a->stat->st_mtime - b->stat->st_mtime;
 	if (a->stat->st_mtime == b->stat->st_mtime)
-		return (a->stat->st_mtimespec.tv_nsec > b->stat->st_mtimespec.tv_nsec);
-	else
-		return (a->stat->st_mtime - b->stat->st_mtime);
+		diff = (a->stat->st_mtimespec.tv_nsec == b->stat->st_mtimespec.tv_nsec)
+			? 1 : a->stat->st_mtimespec.tv_nsec > b->stat->st_mtimespec.tv_nsec;
+	return (diff);
 }
 
 int		name_cmp(t_file *a, t_file *b)
