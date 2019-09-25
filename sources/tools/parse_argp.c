5373d8efa8e3769bb7044576a6c8885c1ecb7eda
4slan <amalsago@student.42.fr>
2019-09-25 10:04:01 +0200

    definition of static fuction that override option

--- sources/tools/parse_argp.c
+++ sources/tools/parse_argp.c
@@ -6,13 +6,21 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 18:31:16 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/17 13:09:40 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/25 10:02:29 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
 extern t_argp	g_argp[];
+
+static void		override(const char option)
+{
+	if (option == '1')
+		g_argp[LONG_FORMAT].active = 0;
+	else if (option == 'l')
+		g_argp[ONE_PER_LINE].active = 0;
+}
 
 void			parse_argp(const char *av)
 {
@@ -28,6 +36,7 @@ void			parse_argp(const char *av)
 		while (g_argp[++k].sign != 0)
 			if (g_argp[k].sign == av[i])
 			{
+				override(av[i]);
 				g_argp[k].active = 1;
 				found = 1;
 			}
