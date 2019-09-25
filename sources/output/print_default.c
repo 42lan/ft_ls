e389d9a7dce79b2b0486014d931a454be1392168
4slan <amalsago@student.42.fr>
2019-09-25 12:40:23 +0200

    removing width variable to normalize code (25 lines)

--- sources/output/print_default.c
+++ sources/output/print_default.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 03:21:49 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/25 12:26:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/25 12:38:11 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 void					print_default(t_dir *directory)
 {
-	size_t				i;
-	size_t				per_row;
-	size_t				per_col;
+	int					i;
+	int					per_row;
+	int					per_col;
 	struct winsize		terminal;
-	int					width;
 
 	i = 0;
 	ioctl(STDIN_FILENO, TIOCGWINSZ, &terminal);
 	per_row = (terminal.ws_col / directory->filename_wmax) - 1;
+	if (per_row < 1)
+		per_row = 1;
 	per_col = (directory->nb_files / per_row) + 1;
 	while (directory->file_head != NULL)
 	{
-		width = (directory->file_head->next && (i != per_row - 1))
-				? directory->filename_wmax + 5 : 0;
-		print_filename(directory->file_head, width);
+		print_filename(directory->file_head, (directory->file_head->next
+					&& (i != per_row - 1)) ? directory->filename_wmax + 5 : 0);
 		directory->file_head = directory->file_head->next;
 		i++;
 		if (i == per_row)
