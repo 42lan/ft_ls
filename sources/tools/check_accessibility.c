ff2d5f86b67a35d2f43cc225bfd706141bd27ccf
4slan <amalsago@student.42.fr>
2019-09-25 10:04:51 +0200

    using new oneline function to print error

--- sources/tools/check_accessibility.c
+++ sources/tools/check_accessibility.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/24 13:40:18 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/22 11:25:17 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/25 09:54:24 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,7 @@ void		check_accessibility(t_file **head)
 	{
 		if (get_stat((*tracer)) == -1)
 		{
-			ft_strerror("ft_ls: ");
-			ft_strerror((*tracer)->name);
-			ft_strerror(" : No such file or directory\n");
+			print_error((*tracer)->name, errno);
 			bad_file = *tracer;
 			remove_file(bad_file);
 			*tracer = (*tracer)->next;
