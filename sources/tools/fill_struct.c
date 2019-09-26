50d21500599f4602352e25a406df3f42cbc72f81
4slan <amalsago@student.42.fr>
2019-09-26 17:45:03 +0200

    name of define was changed

--- sources/tools/fill_struct.c
+++ sources/tools/fill_struct.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 10:28:56 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/25 11:19:10 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/26 16:01:23 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ void				fill_struct(t_file *file)
 	}
 	pwstruct = get_pwstruct(file->stat->st_uid);
 	grstruct = get_grstruct(file->stat->st_gid);
-	if (pwstruct != NULL && !(g_argp[NUMERIC_ID].active))
+	if (pwstruct != NULL && !(g_argp[LONG_NUMERIC_ID].active))
 		file->ownername = pwstruct->pw_name;
 	else
 		file->ownername = ft_itoa(file->stat->st_uid);
-	if (grstruct != NULL && !(g_argp[NUMERIC_ID].active))
+	if (grstruct != NULL && !(g_argp[LONG_NUMERIC_ID].active))
 		file->groupname = grstruct->gr_name;
 	else
 		file->groupname = ft_itoa(file->stat->st_gid);
@@ -38,5 +38,4 @@ void				fill_struct(t_file *file)
 		get_link(file);
 	get_mode(file);
 	get_major_minor(file);
-	append_indicator(file);
 }
