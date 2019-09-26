ed01473507d2a9fc4715821a863b979cbc6d52f9
4slan <amalsago@student.42.fr>
2019-09-26 17:44:42 +0200

    arranging condition in ASCII order

--- sources/tools/parse_argp.c
+++ sources/tools/parse_argp.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 18:31:16 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/25 11:13:25 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/26 16:42:48 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,28 @@ extern t_argp	g_argp[];
 
 static void		override(const char option)
 {
-	if (option == '1')
-		g_argp[LONG_FORMAT].active = 0;
+	if (option == 'F')
+		g_argp[INDICATOR_SLASH].active = 0;
+	else if (option == 'g')
+	{
+		g_argp[LONG_FORMAT].active = 1;
+		g_argp[ONE_PER_LINE].active = 0;
+	}
 	else if (option == 'l')
 		g_argp[ONE_PER_LINE].active = 0;
+	else if (option == 'n')
+		g_argp[LONG_FORMAT].active = 1;
+	else if (option == 'o')
+		g_argp[LONG_FORMAT].active = 1;
 	else if (option == 'p')
 		g_argp[INDICATORS].active = 0;
-	else if (option == 'F')
-		g_argp[INDICATOR_SLASH].active = 0;
+	else if (option == '1')//g_argp[ONE_PER_LINE].sign)
+	{
+		g_argp[LONG_FORMAT].active = 0;
+		g_argp[LONG_NUMERIC_ID].active = 0;
+		g_argp[LONG_WITHOUT_OWNER].active = 0;
+		g_argp[LONG_WITHOUT_GROUP].active = 0;
+	}
 }
 
 void			parse_argp(const char *av)
