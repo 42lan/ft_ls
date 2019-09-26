4dfcc07973faaf336907c3ffe03a2ead0534c1c4
4slan <amalsago@student.42.fr>
2019-09-26 20:24:35 +0200

    handeling case when -g and -o are activated - extra space need to be added

--- sources/output/print_size.c
+++ sources/output/print_size.c
@@ -6,13 +6,17 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 13:32:37 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/07 13:32:47 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/26 20:19:01 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	print_size(off_t st_size, size_t width)
+extern t_argp	g_argp[];
+
+void			print_size(off_t st_size, size_t width)
 {
+	if (g_argp[LONG_WITHOUT_OWNER].active && g_argp[LONG_WITHOUT_GROUP].active)
+		ft_putchar(' ');
 	ft_printf("%*d", width + 2, st_size);
 }
