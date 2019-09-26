86c0dbbecac2b244f30b741c3c3a02b089de838f
4slan <amalsago@student.42.fr>
2019-09-26 17:48:51 +0200

    adding some condition to avoid printing fields if corresponding options was activated

--- sources/output/print_long.c
+++ sources/output/print_long.c
@@ -6,35 +6,41 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/23 14:36:39 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/26 16:22:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+extern t_argp	g_argp[];
+
 void			print_long(t_dir *directory)
 {
 	t_file		*file;
+	size_t		width;
 
 	file = directory->file_head;
 	if (file)
 	{
-		if (file->next != NULL)
+		if (file != NULL)// && S_ISDIR(file->stat->st_mode))
 			print_totalblocks(directory->total_blocks);
 		while (file != NULL)
 		{
 			print_mode(file->mode);
 			print_nlink(file->stat->st_nlink, directory->nlink_wmax);
-			print_ownername(file->ownername, directory->ownername_wmax);
-			print_groupname(file->groupname, directory->groupname_wmax);
-			if (ft_strequ(directory->name, "/dev") && !(S_ISDIR(file->stat->st_mode)))
+			if (g_argp[LONG_WITHOUT_OWNER].active == 0)
+				print_ownername(file->ownername, directory->ownername_wmax);
+			if (g_argp[LONG_WITHOUT_GROUP].active == 0)
+				print_groupname(file->groupname, directory->groupname_wmax);
+			if (ft_strequ(directory->name, "/dev") && !(S_ISDIR(file->stat->st_mode)) && !(S_ISLNK(file->stat->st_mode)))
 				print_major_minor(directory, file->major, file->minor);
 			else
-				print_size(file->stat->st_size,
-						(ft_strequ(directory->name, "/dev"))
-						&& ((S_ISDIR(file->stat->st_mode)
-							|| S_ISLNK(file->stat->st_mode)))
-						? directory->size_wmax + 7 : directory->size_wmax);
+			{
+				width = directory->size_wmax;
+				if (ft_strequ(directory->name, "/dev") && (S_ISDIR(file->stat->st_mode) || S_ISLNK(file->stat->st_mode)))
+					width += 7;
+				print_size(file->stat->st_size, width);
+			}
 			print_mtime(file->stat->st_mtimespec.tv_sec);
 			print_filename(file, 0);
 			file = file->next;
