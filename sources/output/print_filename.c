f40b00da379006c9cb641d5c7115c8277c8dcfad
4slan <amalsago@student.42.fr>
2019-09-26 16:58:14 +0200

    defining function that put offset between files in default mode and callie append_indicator() who puts indicator symbol

--- sources/output/print_filename.c
+++ sources/output/print_filename.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 03:18:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/21 15:36:22 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/26 16:57:12 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,33 @@
 
 extern t_argp	g_argp[];
 
+void			putoffset(int width)
+{
+	while (width--)
+		write(1, " ", 1);
+
+}
+
 void			print_filename(t_file *file, int width)
 {
 	if (g_argp[LONG_FORMAT].active)
 	{
 		if (S_ISLNK(file->stat->st_mode))
-			ft_printf(" %s -> %s", file->name, file->target);
+		{
+			ft_printf(" %s", file->name, file->target);
+			append_indicator(file);
+			ft_printf(" -> %s", file->target);
+		}
 		else
+		{
 			ft_printf(" %s", file->name);
+			append_indicator(file);
+		}
 	}
 	else
-		ft_printf("%-*s", width, file->name);
+	{
+		ft_printf("%s", file->name);
+		append_indicator(file);
+		putoffset(width - file->namlen);
+	}
 }
