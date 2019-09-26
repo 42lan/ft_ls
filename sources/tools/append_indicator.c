584143d265baf9f71a41da46c2594298bc773f69
4slan <amalsago@student.42.fr>
2019-09-26 17:46:14 +0200

    printing corresponding caracter instead concataneting which take more time and can cause error because overwriting \0

--- sources/tools/append_indicator.c
+++ sources/tools/append_indicator.c
@@ -6,22 +6,29 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 11:16:11 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/23 15:05:19 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/26 13:08:49 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	append_indicator(t_file *file)
+extern t_argp	g_argp[];
+
+void			append_indicator(t_file *file)
 {
-	if (S_ISDIR(file->stat->st_mode))
-		ft_strcat(file->name, "/");
-	else if (S_ISREG(file->stat->st_mode) && file->mode[3] == 'x')
-		ft_strcat(file->name, "*");
-	else if (S_ISLNK(file->stat->st_mode))
-		ft_strcat(file->name, "@");
-	else if (S_ISSOCK(file->stat->st_mode))
-		ft_strcat(file->name, "=");
-	else if (S_ISFIFO(file->stat->st_mode))
-		ft_strcat(file->name, "|");
+	if (g_argp[INDICATOR_SLASH].active && S_ISDIR(file->stat->st_mode))
+		ft_putchar('/');
+	else if (g_argp[INDICATORS].active)
+	{
+		if (S_ISDIR(file->stat->st_mode))
+			ft_putchar('/');
+		else if (S_ISREG(file->stat->st_mode) && (FT_XUGO(file->stat->st_mode)))
+			ft_putchar('*');
+		else if (S_ISLNK(file->stat->st_mode))
+			ft_putchar('@');
+		else if (S_ISSOCK(file->stat->st_mode))
+			ft_putchar('=');
+		else if (S_ISFIFO(file->stat->st_mode))
+			ft_putchar('|');
+	}
 }
