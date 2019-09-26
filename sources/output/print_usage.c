ea3e238b98b9e50d28ed53ae77506b7dae86f78e
4slan <amalsago@student.42.fr>
2019-09-26 17:46:30 +0200

    adding forgetted option -g

--- sources/output/print_usage.c
+++ sources/output/print_usage.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 04:24:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/25 11:01:13 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/26 15:51:49 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void		print_usage(char c)
 {
 	ft_strerror("ft_ls: illegal option -- ");
 	ft_putchar_fd(c, STDERR_FILENO);
-	ft_error_exit("\nusage: ft_ls [-AFRalnprt1] [file ...]\n");
+	ft_error_exit("\nusage: ft_ls [-AFRaglnoprt1] [file ...]\n");
 }
