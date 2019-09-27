c2badbee19cad85542ee021e30e20595a29ac99e
4slan <amalsago@student.42.fr>
2019-09-27 11:46:00 +0200

    function reveives wmax istead whole pointer to directory

--- sources/output/print_major_minor.c
+++ sources/output/print_major_minor.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 12:01:43 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/26 16:20:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/27 11:26:59 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 extern t_argp	g_argp[];
 
-void			print_major_minor(t_dir *dir, size_t major, size_t minor)
+void			print_major_minor(t_wmax *wmax, size_t major, size_t minor)
 {
 	if (g_argp[LONG_WITHOUT_OWNER].active)
 		ft_putchar(' ');
-	ft_printf("%*zu, %*zu", dir->major_wmax + 3, major, dir->minor_wmax, minor);
+	ft_printf("%*zu, %*zu", wmax->major + 3, major, wmax->minor, minor);
 }
