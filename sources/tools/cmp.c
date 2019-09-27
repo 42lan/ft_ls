890ebc90692bad6be0e459cdd19107fcaeea29a7
4slan <amalsago@student.42.fr>
2019-09-27 12:34:01 +0200

    refactoring mtime_cmp beacause there is no need to do all this stuff, because using tv_sec directly

--- sources/tools/cmp.c
+++ sources/tools/cmp.c
@@ -6,24 +6,18 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/01 11:38:16 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/23 13:37:43 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/27 12:19:35 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int			mtime_cmp(t_file *a, t_file *b)
+int		mtime_cmp(t_file *a, t_file *b)
 {
-	int		diff;
-
-	diff = a->stat->st_mtime - b->stat->st_mtime;
-	if (a->stat->st_mtime == b->stat->st_mtime)
-		diff = (a->stat->st_mtimespec.tv_nsec == b->stat->st_mtimespec.tv_nsec)
-			? 1 : a->stat->st_mtimespec.tv_nsec > b->stat->st_mtimespec.tv_nsec;
-	return (diff);
+	return (b->stat->st_mtimespec.tv_sec - a->stat->st_mtimespec.tv_sec);
 }
 
-int			name_cmp(t_file *a, t_file *b)
+int		name_cmp(t_file *a, t_file *b)
 {
 	return (ft_strcmp(a->name, b->name));
 }
