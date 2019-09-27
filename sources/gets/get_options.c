21783464c63ab4998373756402f9f7172eab4bb7
4slan <amalsago@student.42.fr>
2019-09-27 13:54:12 +0200

    adding my invented case to print help of my ft_ls

--- sources/gets/get_options.c
+++ sources/gets/get_options.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/11 14:58:40 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/27 12:44:53 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/27 12:51:21 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void		get_options(int argc, char **argv)
 	i = 1;
 	while (i < argc)
 	{
-		if (ft_strequ(argv[i], "--"))
+		if (ft_strequ(argv[i], "--help"))
+			print_help();
+		else if (ft_strequ(argv[i], "--"))
 			break ;
 		else if (is_option(argv[i]))
 			parse_argp(argv[i]);
