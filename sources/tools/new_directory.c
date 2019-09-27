bb2523bfc5150e1b332b78f28ea37364c5c96407
4slan <amalsago@student.42.fr>
2019-09-27 12:04:51 +0200

    defining new static function that initialize wmax structure

--- sources/tools/new_directory.c
+++ sources/tools/new_directory.c
@@ -6,29 +6,35 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 15:39:53 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/22 10:50:13 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/27 12:01:56 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-t_dir		*new_directory(const char *name)
+static t_wmax	*new_wmax(void)
 {
-	t_dir	*directory;
+	t_wmaxi		*wmax;
+
+	if (!(wmax = (t_wmax *)ft_memalloc(sizeof(t_wmax))))
+		return (NULL);
+	return (wmax);
+}
+
+t_dir			*new_directory(const char *name)
+{
+	t_dir		*directory;
 
 	if (!(directory = (t_dir *)ft_memalloc(sizeof(t_dir))))
 		return (NULL);
-	directory->name = ft_strdup(name);
+	directory->name = (name != NULL) ? ft_strdup(name) : NULL;
+	directory->ownername = NULL;
+	directory->groupname = NULL;
 	directory->total_blocks = 0;
-	directory->filename_wmax = 0;
-	directory->nlink_wmax = 0;
-	directory->size_wmax = 0;
-	directory->ownername_wmax = 0;
-	directory->groupname_wmax = 0;
-	directory->major_wmax = 0;
-	directory->minor_wmax = 0;
-	directory->file_head = NULL;
+	directory->nb_files = 0;
+	directory->wmax = new_wmax();
 	directory->subdir_head = NULL;
+	directory->file_head = NULL;
 	directory->next = NULL;
 	return (directory);
 }
