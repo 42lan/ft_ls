8abddba971674187d29422da908cc679e09acbba
4slan <amalsago@student.42.fr>
2019-09-27 11:44:30 +0200

    passing passing value of wmax passing by new created pointer to t_wmax

--- sources/output/print_long.c
+++ sources/output/print_long.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/26 16:22:33 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/27 11:25:23 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,21 @@ void			print_long(t_dir *directory)
 	file = directory->file_head;
 	if (file)
 	{
-		if (file != NULL)// && S_ISDIR(file->stat->st_mode))
+		if (directory->name && file)
 			print_totalblocks(directory->total_blocks);
 		while (file != NULL)
 		{
 			print_mode(file->mode);
-			print_nlink(file->stat->st_nlink, directory->nlink_wmax);
+			print_nlink(file->stat->st_nlink, directory->wmax->nlink);
 			if (g_argp[LONG_WITHOUT_OWNER].active == 0)
-				print_ownername(file->ownername, directory->ownername_wmax);
+				print_ownername(file->ownername, directory->wmax->ownername);
 			if (g_argp[LONG_WITHOUT_GROUP].active == 0)
-				print_groupname(file->groupname, directory->groupname_wmax);
+				print_groupname(file->groupname, directory->wmax->groupname);
 			if (ft_strequ(directory->name, "/dev") && !(S_ISDIR(file->stat->st_mode)) && !(S_ISLNK(file->stat->st_mode)))
-				print_major_minor(directory, file->major, file->minor);
+				print_major_minor(directory->wmax, file->major, file->minor);
 			else
 			{
-				width = directory->size_wmax;
+				width = directory->wmax->size;
 				if (ft_strequ(directory->name, "/dev") && (S_ISDIR(file->stat->st_mode) || S_ISLNK(file->stat->st_mode)))
 					width += 7;
 				print_size(file->stat->st_size, width);
