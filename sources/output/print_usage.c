3304f2297ea70af9d56c20c134adeaa21695794e
4slan <amalsago@student.42.fr>
2019-09-27 13:55:39 +0200

    printing option using while loop to stderr

--- sources/output/print_usage.c
+++ sources/output/print_usage.c
@@ -6,15 +6,23 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 04:24:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/26 15:51:49 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/27 13:03:37 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+extern t_argp	g_argp[];
+
 void		print_usage(char c)
 {
+	int		i;
+
+	i = -1;
 	ft_strerror("ft_ls: illegal option -- ");
 	ft_putchar_fd(c, STDERR_FILENO);
-	ft_error_exit("\nusage: ft_ls [-AFRaglnoprt1] [file ...]\n");
+	ft_strerror("\nusage: ft_ls [-");
+	while (g_argp[++i].sign)
+		ft_putchar_fd(g_argp[i].sign, STDERR_FILENO);
+	ft_error_exit("] [file ...]\n");
 }
