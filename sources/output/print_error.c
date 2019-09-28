efb5ccca4ee6a50eebb6c688c266b0ac1eb6c092
4slan <amalsago@student.42.fr>
2019-09-28 11:47:28 +0200

    using new name of function to print error to stderr

--- sources/output/print_error.c
+++ sources/output/print_error.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 09:52:38 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/25 09:57:32 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/28 11:44:26 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void		print_error(const char *filename, int errnum)
 {
-	ft_strerror("ft_ls: ");
-	ft_strerror(filename);
-	ft_strerror(": ");
-	ft_strerror(strerror(errnum));
-	ft_strerror("\n");
+	ft_print_error("ls: ");
+	ft_print_error(filename);
+	ft_print_error(": ");
+	ft_print_error(strerror(errnum));
+	ft_print_error("\n");
 }
