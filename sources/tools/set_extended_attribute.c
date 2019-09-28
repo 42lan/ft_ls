0151e63a7dfe72ad2550b39da36a18473d0d08f6
4slan <amalsago@student.42.fr>
2019-09-28 13:36:16 +0200

    handeling case if relpath is not setted

--- sources/tools/set_extended_attribute.c
+++ sources/tools/set_extended_attribute.c
@@ -6,17 +6,20 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 10:49:28 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/09 10:50:30 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/28 13:33:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	set_extended_attribute(t_file *file)
+void		set_extended_attribute(t_file *file)
 {
-	if (listxattr(file->relpath, NULL, 0, XATTR_NOFOLLOW) > 0)
+	char	*path;
+
+	path = (file->relpath == NULL) ? file->name : file->relpath;
+	if (listxattr(path, NULL, 0, XATTR_NOFOLLOW) > 0)
 		file->mode[10] = '@';
-	else if (acl_get_file(file->relpath, ACL_TYPE_EXTENDED) != NULL)
+	else if (acl_get_file(path, ACL_TYPE_EXTENDED) != NULL)
 		file->mode[10] = '+';
 	else
 		file->mode[10] = ' ';
