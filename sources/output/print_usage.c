af9a414482d4b22dd9fd958aaa25e5aafdb0f7aa
4slan <amalsago@student.42.fr>
2019-09-28 11:50:08 +0200

    using new name of functions to print error to stderr

--- sources/output/print_usage.c
+++ sources/output/print_usage.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 04:24:15 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/27 13:03:37 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/28 11:43:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ void		print_usage(char c)
 	int		i;
 
 	i = -1;
-	ft_strerror("ft_ls: illegal option -- ");
+	ft_print_error("ft_ls: illegal option -- ");
 	ft_putchar_fd(c, STDERR_FILENO);
-	ft_strerror("\nusage: ft_ls [-");
+	ft_print_error("\nusage: ft_ls [-");
 	while (g_argp[++i].sign)
 		ft_putchar_fd(g_argp[i].sign, STDERR_FILENO);
-	ft_error_exit("] [file ...]\n");
+	ft_print_error_and_exit("] [file ...]\n");
 }
