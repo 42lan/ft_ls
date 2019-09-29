4d9b67a531572271a60f9ff699a343645918468e
4slan <amalsago@student.42.fr>
2019-09-29 13:52:50 +0200

    creation of separate functions to make function normalize

--- sources/output/print_long.c
+++ sources/output/print_long.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 05:19:46 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/27 11:25:23 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/29 13:49:47 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,30 @@
 
 extern t_argp	g_argp[];
 
+void			print_major_minor_or_size(t_dir *directory, t_file *file)
+{
+	int			width;
+	mode_t		st_mode;
+
+	if (is_dev_dir(directory->name) && isnt_dir_and_lnk(file))
+		print_major_minor(directory->wmax, file->major, file->minor);
+	else
+	{
+		width = directory->wmax->size;
+		st_mode = file->stat->st_mode;
+		print_size(file->stat->st_size, ((is_dev_dir(directory->name))
+					&& (S_ISDIR(st_mode) || S_ISLNK(st_mode)))
+				? width + 7 : width);
+	}
+}
+
 void			print_long(t_dir *directory)
 {
 	t_file		*file;
-	size_t		width;
+	t_wmax		*wmax;
 
 	file = directory->file_head;
+	wmax = directory->wmax;
 	if (file)
 	{
 		if (directory->name && file)
@@ -27,20 +45,10 @@ void			print_long(t_dir *directory)
 		while (file != NULL)
 		{
 			print_mode(file->mode);
-			print_nlink(file->stat->st_nlink, directory->wmax->nlink);
-			if (g_argp[LONG_WITHOUT_OWNER].active == 0)
-				print_ownername(file->ownername, directory->wmax->ownername);
-			if (g_argp[LONG_WITHOUT_GROUP].active == 0)
-				print_groupname(file->groupname, directory->wmax->groupname);
-			if (ft_strequ(directory->name, "/dev") && !(S_ISDIR(file->stat->st_mode)) && !(S_ISLNK(file->stat->st_mode)))
-				print_major_minor(directory->wmax, file->major, file->minor);
-			else
-			{
-				width = directory->wmax->size;
-				if (ft_strequ(directory->name, "/dev") && (S_ISDIR(file->stat->st_mode) || S_ISLNK(file->stat->st_mode)))
-					width += 7;
-				print_size(file->stat->st_size, width);
-			}
+			print_nlink(file->stat->st_nlink, wmax->nlink);
+			print_ownername(file->ownername, wmax->ownername);
+			print_groupname(file->groupname, wmax->groupname);
+			print_major_minor_or_size(directory, file);
 			print_mtime(file->stat->st_mtimespec.tv_sec);
 			print_filename(file, 0);
 			file = file->next;
