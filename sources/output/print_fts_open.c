dc7737fa7ea06a7c3e882162bce9b8533fb79f19
4slan <amalsago@student.42.fr>
2019-09-29 12:24:30 +0200

    renaming print_error() into print_ft_ls_error() to avoid all confusions

--- sources/output/print_fts_open.c
+++ sources/output/print_fts_open.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/28 10:48:58 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/29 12:15:37 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/29 12:20:08 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	print_fts_open(void)
 {
-	print_error("fts_open", ENOENT);
+	print_ft_ls_error("fts_open", ENOENT);
 	exit(EXIT_FAILURE);
 }
