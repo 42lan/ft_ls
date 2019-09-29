b42774a910d1d33e71d315f447003ed2772a18d6
4slan <amalsago@student.42.fr>
2019-09-29 12:14:18 +0200

    normalization

--- sources/gets/get_argument_files.c
+++ sources/gets/get_argument_files.c
@@ -6,11 +6,22 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 14:27:39 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/27 12:46:34 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/29 12:09:46 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+static void		append_to_head(t_file **head, t_file *file)
+{
+	if ((*head) == NULL)
+		(*head) = file;
+	else
+	{
+		file->next = (*head);
+		(*head) = file;
+	}
+}
 
 t_file			*get_argument_files(int argc, char **argv)
 {
@@ -18,30 +29,23 @@ t_file			*get_argument_files(int argc, char **argv)
 	t_file		*head;
 	t_file		*file;
 
-	i = 1;
+	i = 0;
 	head = NULL;
-	while (is_option(argv[i]))
-	{
+	while (is_option(argv[++i]))
 		if (ft_strequ(argv[i], "--"))
 		{
 			i++;
 			break ;
 		}
-		i++;
-	}
 	if (argv[i] == NULL)
 		head = new_file(NULL, ".");
 	else
 		while (i < argc)
 		{
+			if (ft_strequ(argv[i], ""))
+				print_fts_open();
 			file = new_file(NULL, argv[i]);
-			if (head == NULL)
-				head = file;
-			else
-			{
-				file->next = head;
-				head = file;
-			}
+			append_to_head(&head, file);
 			i++;
 		}
 	return (head);
