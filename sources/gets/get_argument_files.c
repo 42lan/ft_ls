6ca32746a6879e4bcb853fa70ab739f9a103e8e7
4slan <amalsago@student.42.fr>
2019-09-30 11:15:59 +0200

    refactoring by creating directry to hold files from argv and their number

--- sources/gets/get_argument_files.c
+++ sources/gets/get_argument_files.c
@@ -6,31 +6,20 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 14:27:39 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/29 12:09:46 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/30 09:55:48 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static void		append_to_head(t_file **head, t_file *file)
-{
-	if ((*head) == NULL)
-		(*head) = file;
-	else
-	{
-		file->next = (*head);
-		(*head) = file;
-	}
-}
-
-t_file			*get_argument_files(int argc, char **argv)
+t_dir			*get_argument_files(int argc, char **argv)
 {
 	int			i;
-	t_file		*head;
+	t_dir		*directory;
 	t_file		*file;
 
 	i = 0;
-	head = NULL;
+	directory = new_directory(NULL);
 	while (is_option(argv[++i]))
 		if (ft_strequ(argv[i], "--"))
 		{
@@ -38,15 +27,15 @@ t_file			*get_argument_files(int argc, char **argv)
 			break ;
 		}
 	if (argv[i] == NULL)
-		head = new_file(NULL, ".");
+		directory->file_head = new_file(NULL, ".");
 	else
 		while (i < argc)
 		{
 			if (ft_strequ(argv[i], ""))
 				print_fts_open();
 			file = new_file(NULL, argv[i]);
-			append_to_head(&head, file);
+			append_file(&directory, file);
 			i++;
 		}
-	return (head);
+	return (directory);
 }
