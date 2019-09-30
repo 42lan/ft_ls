4961367ae6ce470b588fb0ce11e876d248e43f24
4slan <amalsago@student.42.fr>
2019-09-30 11:15:02 +0200

    removing check_for_one_dir() beacase using value of nb_files in directory{

--- sources/ft_ls.c
+++ sources/ft_ls.c
@@ -6,30 +6,13 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 11:02:03 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/29 19:43:00 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/09/30 10:57:46 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
 extern t_argp	g_argp[];
-
-static int		check_for_one_dir(t_file *head)
-{
-	t_file		*tmp;
-	int			one_dir;
-
-	tmp = head;
-	one_dir = 1;
-	while (tmp && one_dir == 1)
-	{
-		if (S_ISDIR(tmp->stat->st_mode))
-			if (tmp->next && S_ISDIR(tmp->next->stat->st_mode))
-				one_dir = 0;
-		tmp = tmp->next;
-	}
-	return (one_dir);
-}
 
 static void		nondirtypes_handler(t_file *head)
 {
@@ -45,23 +28,19 @@ static void		nondirtypes_handler(t_file *head)
 		fill_struct(file);
 		determine_wmax(directory->wmax, file);
 		append_file(&directory, file);
-		directory->nb_files++;
 		head = head->next;
 	}
 	apply_options(directory);
 	display(directory);
 }
 
-static void		directories_handler(t_file *head)
+static void		directories_handler(t_file *head, size_t nb_files)
 {
-	int			one_dir;
-
-	one_dir = check_for_one_dir(head);
 	while (head)
 	{
 		if (S_ISDIR(head->stat->st_mode))
 		{
-			(one_dir != 1) ? ft_printf("%s:\n", head->name) : 0;
+			(nb_files > 1) ? ft_printf("%s:\n", head->name) : 0;
 			browse_directory(head->name);
 		}
 		head = head->next;
@@ -70,13 +49,16 @@ static void		directories_handler(t_file *head)
 	}
 }
 
-int				ft_ls(t_file *head)
+int				ft_ls(t_dir *head)
 {
-	if (!head)
+	t_file		*file_list;
+
+	if (head == NULL || head->file_head == NULL)
 		return (0);
-	if (!S_ISDIR(head->stat->st_mode))
-		nondirtypes_handler(head);
-	if (head)
-		directories_handler(head);
+	file_list = head->file_head;
+	if (!S_ISDIR(file_list->stat->st_mode))
+		nondirtypes_handler(file_list);
+	if (file_list)
+		directories_handler(file_list, head->nb_files);
 	return (1);
 }
