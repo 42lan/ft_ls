2fc3687a5b2178d691f0f3da94a3b3f46c62ec9f
4slan <amalsago@student.42.fr>
2019-10-01 14:24:03 +0200

    removing malloc for mode because it is an array of 12 chars; accessing to stat's fields by point because malloc for stat was removed

--- sources/gets/get_mode.c
+++ sources/gets/get_mode.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 13:45:50 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/09 10:50:33 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/01 13:42:40 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 void	get_mode(t_file *file)
 {
-	file->mode = ft_memalloc(12);
-	file->mode[0] = get_type(file->stat->st_mode);
-	ft_strcat(file->mode, get_permissions(file->stat->st_mode, 0));
-	ft_strcat(file->mode, get_permissions(file->stat->st_mode, 3));
-	ft_strcat(file->mode, get_permissions(file->stat->st_mode, 6));
-	set_special_permissions(file->stat->st_mode, &file->mode);
+	file->mode[0] = get_type(file->stat.st_mode);
+	ft_strcat(file->mode, get_permissions(file->stat.st_mode, 0));
+	ft_strcat(file->mode, get_permissions(file->stat.st_mode, 3));
+	ft_strcat(file->mode, get_permissions(file->stat.st_mode, 6));
+	set_special_permissions(file->stat.st_mode, file->mode);
 	set_extended_attribute(file);
 }
