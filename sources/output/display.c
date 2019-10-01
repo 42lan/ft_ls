0bf77bbf58de6ad4fd0745cec7e3bb774882c57b
4slan <amalsago@student.42.fr>
2019-10-01 14:26:19 +0200

    removing comment of print functions

--- sources/output/display.c
+++ sources/output/display.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 21:07:44 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/30 15:34:47 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/01 13:28:19 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ extern t_argp	g_argp[];
 
 void			display(t_dir *directory)
 {
-	(void)directory;
-	/*
 	if (g_argp[LONG_FORMAT].active)
 		print_long(directory);
 	else if (g_argp[ONE_PER_LINE].active)
 		print_one_per_line(directory);
 	else
 		print_default(directory);
-	*/
 }
