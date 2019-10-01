ee2fd047b938ca3f87f48eba79882fd8af96b1d3
4slan <amalsago@student.42.fr>
2019-10-01 14:29:59 +0200

    renaming str to mode; received mode is no more poiter to pointer

--- sources/tools/set_special_permissions.c
+++ sources/tools/set_special_permissions.c
@@ -6,42 +6,42 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 18:41:43 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/05 18:42:08 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/01 14:29:19 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static void		set_suid(char **str)
+static void		set_suid(char **mode)
 {
-	if ((*str)[3] == 'x')
-		(*str)[3] = 's';
-	else if ((*str)[3] == '-')
-		(*str)[3] = 'S';
+	if ((*mode)[3] == 'x')
+		(*mode)[3] = 's';
+	else if ((*mode)[3] == '-')
+		(*mode)[3] = 'S';
 }
 
-static void		set_sgid(char **str)
+static void		set_sgid(char **mode)
 {
-	if ((*str)[6] == 'x')
-		(*str)[6] = 's';
-	else if ((*str)[6] == '-')
-		(*str)[6] = 'S';
+	if ((*mode)[6] == 'x')
+		(*mode)[6] = 's';
+	else if ((*mode)[6] == '-')
+		(*mode)[6] = 'S';
 }
 
-static void		set_stickybit(char **str)
+static void		set_stickybit(char **mode)
 {
-	if ((*str)[9] == 'x')
-		(*str)[9] = 't';
-	else if ((*str)[9] == '-')
-		(*str)[9] = 'T';
+	if ((*mode)[9] == 'x')
+		(*mode)[9] = 't';
+	else if ((*mode)[9] == '-')
+		(*mode)[9] = 'T';
 }
 
-void			set_special_permissions(mode_t st_mode, char **str)
+void			set_special_permissions(mode_t st_mode, char *mode)
 {
 	if (st_mode & S_ISUID)
-		set_suid(str);
+		set_suid(&mode);
 	if (st_mode & S_ISGID)
-		set_sgid(str);
+		set_sgid(&mode);
 	if (st_mode & S_ISVTX)
-		set_stickybit(str);
+		set_stickybit(&mode);
 }
