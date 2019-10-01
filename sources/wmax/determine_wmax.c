4b078349747abf8037ab56449d63a87d8dcdce5e
4slan <amalsago@student.42.fr>
2019-10-01 20:22:34 +0200

    adding condition to call function to determine width

--- sources/wmax/determine_wmax.c
+++ sources/wmax/determine_wmax.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 15:05:30 by amalsago          #+#    #+#             */
-/*   Updated: 2019/09/27 11:11:26 by amalsago         ###   ########.fr       */
+/*   Updated: 2019/10/01 19:16:40 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@
 ** from stat struct.
 */
 
-void	determine_wmax(t_wmax *wmax, t_file *file)
+extern t_argp	g_argp[];
+
+void			determine_wmax(t_wmax *wmax, t_file *file)
 {
 	determine_filename_wmax(wmax, file);
-	determine_ownername_wmax(wmax, file);
-	determine_groupname_wmax(wmax, file);
-	determine_nlink_wmax(wmax, file);
-	determine_size_wmax(wmax, file);
-	determine_major_minor_wmax(wmax, file);
+	if (g_argp[LONG_FORMAT].active)
+	{
+		determine_ownername_wmax(wmax, file);
+		determine_groupname_wmax(wmax, file);
+		determine_nlink_wmax(wmax, file);
+		determine_size_wmax(wmax, file);
+		determine_major_minor_wmax(wmax, file);
+	}
 }
